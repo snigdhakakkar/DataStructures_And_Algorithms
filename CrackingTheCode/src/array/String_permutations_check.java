3a8505cc6d62dd305ca002baa86c785be9bbaf18
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-01-05 14:06:24 -0500

    Check if the two strings are permutations of each other

---
+++ CrackingTheCode/src/array/String_permutations_check.java
@@ -0,0 +1,35 @@
+package array;
+
+public class String_permutations_check {
+	
+	//Assumptions: permutation comparison is case sensitive and white space is significant in it
+	//'God' is not a permutation of 'dog'
+	//'god    ' is not a permutation of 'dog'
+	
+	
+	public static String sort(String s) {
+		char[] content = s.toCharArray();
+		java.util.Arrays.sort(content);
+		return new String(content);
+	}
+	
+	public static boolean permutation(String s, String t) {
+		
+		if (s.length() != t.length()) {
+			return false;
+		}
+		
+		return sort(s).equals(sort(t));
+	}
+	
+	public static void main(String[] args) {
+		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
+		
+		for(String[] pair:pairs) {
+			String word1 = pair[0];
+			String word2 = pair[1];
+			boolean anagram = permutation(word1, word2);
+			System.out.println(word1 + ", " + word2 + ": " + anagram);
+		}
+	}
+}
