0070d5635d5c3709d91d077bb8b671a659b9bbf4
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-01-06 15:00:33 -0500

    Checking if the string is a permutation of a palindrome
    - Complexity is still O(N) because to check if it's a permutation, we need to scan through the string. But, we have optimized it in such a way, that it checks for odd count for each character as the code flows through the string rather than at the end

---
+++ CrackingTheCode/src/array/IsPalindromePermuation_optimized.java
@@ -0,0 +1,46 @@
+package array;
+
+public class IsPalindromePermuation_optimized {
+	
+	public static boolean isPermutationOfPalindrome(String phrase) {
+		int countOdd = 0;
+		int[] table = new int[Character.getNumericValue('z') -
+		                      Character.getNumericValue('a') + 1];
+		for (char ch: phrase.toCharArray()) {
+			int x = getCharNumber(ch);
+			
+			if (x != -1) {
+				table[x]++;
+				
+				if (table[x]%2 == 1) {
+					countOdd++;
+				} else {
+					countOdd--;
+				}
+				
+			}
+		}
+		return (countOdd <=1);
+	}
+
+	
+	/*Mapping each character to a number, for example a->0; b -> 1; c->2 etc.*/
+	/*Assumption: this is case insensitive. Non-letter characters map to -1 */
+	public static int getCharNumber(Character ch) {
+		int a = Character.getNumericValue('a');
+		int z = Character.getNumericValue('z');
+		int val = Character.getNumericValue(ch);
+		if (a <= val && val <=z ) {
+			return val - a;
+		}
+		
+		return -1;
+	}
+	
+	//driver method
+	public static void main(String[] args) {
+		String pali = "Rats live on no evil star";
+		System.out.println(isPermutationOfPalindrome(pali));
+	}
+
+}
