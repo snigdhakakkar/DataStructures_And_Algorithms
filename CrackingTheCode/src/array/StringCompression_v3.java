30a8f0a46a9031e5356d774db007b486470c32e7
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-01-07 14:36:43 -0500

    String compression program optimized
    - checking the length of compressed string in advance even before compressing
    - will avoid us having to create a string that we would never use
    - we can initialize the StringBuilder to its necessary capacity up-front
    
    - downside: creates a second loop of characters and nearly duplicated code is run

---
+++ CrackingTheCode/src/array/StringCompression_v3.java
@@ -0,0 +1,52 @@
+ package array;
+
+public class StringCompression_v3 {
+	
+	public static String compress(String str) {
+		/* check final length and return input string if it would be longer */
+		int finalLength = countCompression(str);
+		
+		if(finalLength >= str.length()) {
+			return str;
+		}
+		
+		StringBuilder compressed = new StringBuilder(finalLength); //initial capacity
+		int countConsecutive  = 0;
+		for (int i = 0; i < str.length(); i++) {
+			countConsecutive++;
+			
+			/*If next character is different than current, append this char to result*/
+			
+			if(i+1 >= str.length() || str.charAt(i) != str.charAt(i+1)) {
+				compressed.append(str.charAt(i));
+				compressed.append(countConsecutive);
+				countConsecutive = 0;
+			}
+		}
+		
+		return compressed.toString();
+	}
+	
+	public static int countCompression(String str) {
+		int compressedLength = 0;
+		int countConsecutive = 0;
+		for (int i = 0; i < str.length(); i++) {
+			countConsecutive++;
+			
+			/*If next character is different than current,increase the length*/
+			if (i+1 >=str.length() || str.charAt(i) != str.charAt(i+1)) {
+				compressedLength += 1 + String.valueOf(countConsecutive).length();
+				countConsecutive = 0;
+			}
+		}
+		
+		return compressedLength;
+	}
+	
+	public static void main(String[] args) {
+		String str = "aabbb";
+		System.out.println(str);
+		System.out.println(compress(str));
+	}
+
+}
