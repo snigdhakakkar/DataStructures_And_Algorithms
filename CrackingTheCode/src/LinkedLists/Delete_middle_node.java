743b0cc4aac441b6706f8812cc9dfdb768acaad2
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-01-10 11:38:28 -0500

    Program to delete any middle node when we do not have access to the head of the LinkedList

---
+++ CrackingTheCode/src/LinkedLists/Delete_middle_node.java
@@ -0,0 +1,45 @@
+package LinkedLists;
+
+public class Delete_middle_node {
+	
+	public static boolean deleteNode(LinkedListNode n) {
+		
+		if (n == null || n.next == null) {
+			return false;
+		}
+		
+		LinkedListNode next = n.next;
+		n.data = next.data;
+		n.next = next.next;
+		return true;
+	}
+	
+	public static int randomInt(int n) {
+		return (int) (Math.random() * n);
+	}
+
+	public static int randomIntInRange(int min, int max) {
+		return randomInt(max + 1 - min) + min;
+	}
+	
+	public static LinkedListNode randomLinkedList(int N, int min, int max) {
+		LinkedListNode root = new LinkedListNode(randomIntInRange(min, max),
+				null, null);
+		LinkedListNode prev = root;
+		for (int i = 1; i < N; i++) {
+			int data = randomIntInRange(min, max);
+			LinkedListNode next = new LinkedListNode(data, null, null);
+			prev.setNext(next);
+			prev = next;
+		}
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		LinkedListNode head = randomLinkedList(10, 0, 10);
+		System.out.println(head.printForward());
+		deleteNode(head.next.next.next.next); // delete node 4
+		System.out.println(head.printForward());
+	}
+
+}
---
+++ CrackingTheCode/src/array/URLify.java
@@ -0,0 +1,65 @@
+package array;
+
+public class URLify {
+	
+	//Assuming that the string has sufficient free space in the end
+	
+	public static void replaceSpaces(char[] str, int truelength) {
+		int spaceCount = 0, index = 0, i = 0;
+		for (i = 0; i < truelength; i++) {
+			
+			if(str[i] == ' ') {
+				spaceCount++;
+			}
+		}
+		
+		index = truelength + spaceCount * 2;
+		
+		if (truelength < str.length) {
+			str[truelength] = '\0'; //Replacing additional spaces in the end with null character
+		}
+		
+		for (i = truelength - 1; i >= 0; i-- ) {
+			
+			if (str[i] == ' ') {
+				str[index - 1] = '0';
+				str[index - 2] = '2';
+				str[index - 3] = '%';
+				index = index - 3;
+			}
+			
+			else {
+				str[index - 1] = str[i];
+				index--;
+			}
+		}
+	
+	}
+	
+	public static int findLastCharacter(char[] str) {
+		for (int i = str.length - 1; i >= 0; i--) {
+			
+			if (str[i] != ' ') {
+				return i;
+			}
+		}
+		
+		return -1;
+		
+	}
+	
+	public static String charArrayToString(char[] arr) {
+		return new String(arr);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		String str = "Mr. John Smith   ";
+		char[] arr = str.toCharArray();
+		int truelength = findLastCharacter(arr) + 1;
+		replaceSpaces(arr, truelength);
+		System.out.println("\"" + charArrayToString(arr) + "\"");
+	}
+
+}
