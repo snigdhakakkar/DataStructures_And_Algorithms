12d4a33f8be5c83de87687526212739f65738615
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-01-24 12:10:11 -0500

    Program to do binarysearch for a sorted array
    Time complexity: O(logN)

---
+++ LeetCode/src/array/BinarySearch.java
@@ -0,0 +1,35 @@
+package array;
+
+public class BinarySearch {
+	
+	public static void main(String[] args) {
+		//needs a sorted array only
+		int a[] = {12, 34, 45, 65, 83};
+		BinarySearch b = new BinarySearch();
+		int i = b.searchIndex(a, 83, 0, a.length -1);
+		if(i == -1) {
+			System.out.println("value does not exist in the array.");
+		} else {
+			System.out.println(i);
+		}
+	}
+	
+	public static int searchIndex(int[] a, int val, int start, int end) {
+		if(a.length == 0 || start > end) {
+			return -1;
+		}
+		
+		int mid = (start + end)/2;
+		
+		if(val == a[mid]) {
+			return mid;
+		}
+		
+		if(val < a[mid]) {
+			return searchIndex(a, val, start, mid -1);
+		} else {
+			return searchIndex(a, val, mid +1, end);
+		}
+	}
+
+}
