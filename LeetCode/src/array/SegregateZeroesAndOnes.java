36861e9861848f1241d96a9a6fea49d138bdfbd4
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-01-30 19:11:20 -0500

    Code update

--- LeetCode/src/array/SegregateZeroesAndOnes.java
+++ LeetCode/src/array/SegregateZeroesAndOnes.java
@@ -25,11 +25,11 @@ public class SegregateZeroesAndOnes {
 		right = arr.length - 1;
 		
 		while(left < right) {
-			while(arr[left] == 0 && left < right) {
+			while(arr[left] % 2 == 0 && left < right) {
 				left++;
 			}
 			
-			while(arr[right] != 0 && left < right) {
+			while(arr[right] % 2 == 1 && left < right) {
 				right--;
 			}
 			
