74df4a0ebb0259ffacf4592f5a2779b08da159a6
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-01-31 03:55:52 -0500

    Demo test program

---
+++ LeetCode/src/Strings/PrintTheString.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class PrintTheString {
+	
+	public static void main(String[] args) {
+		
+		String s = "Hello world";
+		
+		System.out.println(s);
+		System.out.println(s);
+		
+		String name = "workDoneRight";
+		System.out.println("Name: " + name);
+	}
+}
