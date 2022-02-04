0d33fe568e3bae133113c889fb47bf4c51a36da2
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-04 04:58:30 -0500

    Program to get minimum element with O(1) complexity using two stacks

---
+++ LeetCode/src/Stacks/GetMinElementInBigOOf1.java
@@ -0,0 +1,65 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class GetMinElementInBigOOf1 {
+	
+	Stack<Integer> s1 = new Stack<>();
+	Stack<Integer> s2 = new Stack<>();
+	int min = Integer.MAX_VALUE;
+	
+	public void pushElementInStack(int val) {
+		s1.push(val);
+		
+		if(s2.isEmpty()) {
+			s2.push(val);
+		} else {
+			if(val < s2.peek()) {
+				s2.push(val);
+			}
+		}
+	}
+	
+	public int popElementFromStack() {
+		int val = s1.pop();
+		
+		if(val == s2.peek()) {
+			s2.pop();
+		}
+		return val;
+	}
+	
+	public int minElement() {
+		if(s2.isEmpty()) {
+			return -1;
+		} 
+		
+		return s2.peek();
+	}
+	
+	public static void main(String[] args) {
+		GetMinElementInBigOOf1 a = new GetMinElementInBigOOf1();
+		
+		int[] arr = {7, 8, 3, 1, 5};
+		
+		for(int i = 0; i < arr.length; i++) {
+			a.pushElementInStack(arr[i]);
+		}
+		
+		System.out.print(a.minElement() + " ");
+		
+		a.popElementFromStack();
+		System.out.print(a.minElement() + " ");
+		
+		a.popElementFromStack();
+		System.out.print(a.minElement() + " ");
+		
+		a.popElementFromStack();
+		System.out.print(a.minElement() + " ");
+		
+		a.popElementFromStack();
+		System.out.print(a.minElement() + " ");
+		
+	}
+	
+}
