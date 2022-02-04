91a8af58a49d0fa655239d27b55055b67e96d399
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-04 04:56:05 -0500

    Program to find next larger value in an array using stacks

--- LeetCode/src/Stacks/NextLargerValueOfEachArray.java
+++ LeetCode/src/Stacks/NextLargerValueOfEachArray.java
@@ -1,5 +1,38 @@
 package Stacks;
 
-public class NextLargerValueOfEachArray {
+import java.util.Stack;
 
+public class NextLargerValueOfEachArray {
+	
+	public static void main(String[] args) {
+		
+		NextLargerValueOfEachArray a = new NextLargerValueOfEachArray();
+		
+		int[] arr = {1, 4, 2, 6, 1, 8, 3};
+		
+		a.nextLargerValueOfEachArray(arr);
+	}
+	
+	public void nextLargerValueOfEachArray(int[] arr) {
+		if(arr.length == 0) {
+			return;
+		}
+		
+		Stack<Integer> s = new Stack<Integer>();
+		
+		s.push(arr[0]);
+		
+		for(int i = 1; i < arr.length; i++) {
+			while(!s.isEmpty() && arr[i] > s.peek()) {
+				s.pop();
+				System.out.println(arr[i] + " ");
+			}
+			
+			s.push(arr[i]);
+		}
+		while(!s.isEmpty()) {
+			System.out.println(-1);
+			s.pop();
+		}
+	}
 }
