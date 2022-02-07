28866d874e7c4d62fe20f681a8f67e14b87d0e5b
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-07 06:35:43 -0500

    Abstract animal class for solving the Animal shelter queue question

---
+++ CrackingTheCode/src/Stacks_And_Queues/Animal.java
@@ -0,0 +1,26 @@
+package Stacks_And_Queues;
+
+public abstract class Animal {
+	private int order; 
+	protected String name;
+	
+	public Animal(String n) {
+		name = n;
+	}
+	
+	public abstract String name();
+	
+	public void setOrder(int ord) {
+		order = ord;
+	}
+	
+	public int getOrder() {
+		return order;
+	}
+	
+	public boolean isOlderThan(Animal a) {
+		return this.order < a.getOrder();
+	}
+	
+
+}
