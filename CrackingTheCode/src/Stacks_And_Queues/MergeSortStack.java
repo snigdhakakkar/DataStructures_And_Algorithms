f007c01b209b0f82a3cacb16b73e2f653cbedcd9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-07 06:12:48 -0500

    Another way to sort (mergesort) elements in a stack with improved time complexity

---
+++ CrackingTheCode/src/Stacks_And_Queues/MergeSortStack.java
@@ -0,0 +1,75 @@
+package Stacks_And_Queues;
+
+import java.util.Stack;
+
+public class MergeSortStack {
+	
+	public static void main(String [] args) {
+		Stack<Integer> s = new Stack<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			int r = randomIntInRange(0,  1000);
+			s.push(r);
+		}
+		
+		mergesort(s);
+		
+		while(!s.isEmpty()) {
+			System.out.println(s.pop());
+		}
+	}
+	
+	public static int randomIntInRange(int min, int max) {
+		return randomInt(max + 1 - min) + min;
+	}
+	
+	public static int randomInt(int n) {
+		return (int) (Math.random() * n);
+	}
+	
+	public static Stack<Integer> mergesort(Stack<Integer> inStack){
+		if (inStack.size() <= 1) {
+			return inStack;
+		}
+		
+		Stack<Integer> left = new Stack<Integer>();
+		Stack<Integer> right = new Stack<Integer>();
+		int count = 0;
+		
+		while (inStack.size() != 0) {
+			count++;
+			if (count % 2 == 0) {
+				left.push(inStack.pop());
+			} else {
+				right.push(inStack.pop());
+			}
+		}
+		
+		left = mergesort(left);
+		right = mergesort(right);
+		
+		while (left.size() > 0 || right.size() > 0) {
+			if (left.size() == 0) {
+				inStack.push(right.pop());
+			} else if (right.size() == 0) {
+				inStack.push(left.pop());
+			} else if (right.peek().compareTo(left.peek()) <= 0) {
+				inStack.push(left.pop());
+			} else {
+				inStack.push(right.pop());
+			}
+		}
+		
+		Stack<Integer> reverseStack = new Stack<Integer>();
+		while (inStack.size() > 0) {
+			reverseStack.push(inStack.pop());
+		}
+		return reverseStack;
+	}
+	
+	
+	
+	
+	
+
+}
