b18edd10ff5a957c99fade44ce3e31ba538a181c
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-08 13:25:45 -0500

    Added inorder, preorder, postorder traversal methods for binary tree

--- LeetCode/src/BinaryTree/BinaryTree.java
+++ LeetCode/src/BinaryTree/BinaryTree.java
@@ -11,6 +11,37 @@ public class BinaryTree {
 		return a;
 	}
 	
+	public void inorder(Node node) {
+		if(node == null) {
+			return;
+		}
+		
+		inorder(node.left);
+		System.out.print(node.data + " ");
+		inorder(node.right);
+		
+	}
+	
+	public void preorder(Node node) {
+		if(node == null) {
+			return;
+		}
+		
+		System.out.print(node.data + " ");
+		preorder(node.left);
+		preorder(node.right);
+	}
+	
+	public void postorder(Node node) {
+		if(node == null) {
+			return;
+		}
+		
+		postorder(node.left);
+		postorder(node.right);
+		System.out.print(node.data + " ");
+	}
+	
 	
 
 }
