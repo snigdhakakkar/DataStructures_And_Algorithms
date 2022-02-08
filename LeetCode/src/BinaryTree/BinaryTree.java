3ad2c4e27964da9478b4870dc4452fbcc0ef0529
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-08 13:04:52 -0500

    Basic class for binary tree operations -
    1. Creation of binary tree

---
+++ LeetCode/src/BinaryTree/BinaryTree.java
@@ -0,0 +1,16 @@
+package BinaryTree;
+
+public class BinaryTree {
+	
+	public Node createNewNode(int val) {
+		Node a = new Node();
+		
+		a.data = val;
+		a.left = null;
+		a.right = null;
+		return a;
+	}
+	
+	
+
+}
