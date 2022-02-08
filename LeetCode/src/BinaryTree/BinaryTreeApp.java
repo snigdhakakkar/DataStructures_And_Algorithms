2a1bfb3044a42e51015ad57d1846e5182e260278
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-08 13:05:57 -0500

    Binary tree app

---
+++ LeetCode/src/BinaryTree/BinaryTreeApp.java
@@ -0,0 +1,18 @@
+package BinaryTree;
+
+public class BinaryTreeApp {
+	
+	public static void main(String[] args) {
+		BinaryTree a = new BinaryTree();
+		Node root = a.createNewNode(2);
+		root.left = a.createNewNode(7);
+		root.right = a.createNewNode(5);
+		root.left.left = a.createNewNode(2);
+		root.left.right = a.createNewNode(6);
+		root.left.right.left = a.createNewNode(5);
+		root.left.right.right = a.createNewNode(11);
+		root.right.right = a.createNewNode(9);
+		root.right.right.left = a.createNewNode(4);
+	}
+
+}
