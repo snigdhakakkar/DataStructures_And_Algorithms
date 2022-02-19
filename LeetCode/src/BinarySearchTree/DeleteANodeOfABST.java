a28c90e5f262b5eb5556000c9409a7f0c990691d
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-18 23:25:33 -0500

    delete a node in a binary search tree

---
+++ LeetCode/src/BinarySearchTree/DeleteANodeOfABST.java
@@ -0,0 +1,67 @@
+package BinarySearchTree;
+
+public class DeleteANodeOfABST {
+	
+	public static void main(String[] args) {
+		
+		CreateBinarySearchTree a = new CreateBinarySearchTree();
+		Node root = null;
+		//8, 3, 6, 10, 4, 7, 1, 14, 13
+		root = a.insert(root, 8);
+		root = a.insert(root, 3);
+		root = a.insert(root, 6);
+		root = a.insert(root, 10);
+		root = a.insert(root, 4);
+		root = a.insert(root, 7);
+		root = a.insert(root, 1);
+		root = a.insert(root, 14);
+		root = a.insert(root, 13);
+		
+		root = delete(root, 1);
+		
+	}
+	
+	public static Node delete(Node node, int val) {
+		if(node == null) {
+			return null;
+		}
+		
+		if(val < node.data) {
+			node.left = delete(node.left, val);
+		} else if (val > node.data){
+			node.right = delete(node.right, val);
+		} else {
+			if(node.left == null && node.right == null) {
+				Node temp = null;
+				temp = node.left == null ? node.right : node.left;
+				
+				if(temp == null) {
+					return null;
+				} else {
+					return temp;
+				}
+			} else {
+				Node successor = getSuccessor(node);
+				node.data = successor.data;
+				node.right = delete(node.right, 4);
+			}
+		}
+		return node;
+		
+	}
+	
+	public static Node getSuccessor(Node node) {
+		if(node == null) {
+			return null;
+		}
+		
+		Node temp = node.right;
+		
+		while(temp.left != null) {
+			temp = temp.left;
+		}
+		
+		return temp;
+	}
+
+}
