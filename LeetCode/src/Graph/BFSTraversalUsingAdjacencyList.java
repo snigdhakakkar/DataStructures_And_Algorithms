ca4215f738326c3acdba5fa6ad31202a66df1c08
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-22 05:00:37 -0500

    BFS traversal using adjacency list

---
+++ LeetCode/src/Graph/BFSTraversalUsingAdjacencyList.java
@@ -0,0 +1,24 @@
+package Graph;
+
+public class BFSTraversalUsingAdjacencyList {
+	
+	//time complexity: O(V+E), Space complexity: O(V+E)
+	public static void main(String[] args) {
+		int nodes = 6;
+		
+		Graph_bfsAdjList a = new Graph_bfsAdjList(nodes);
+		
+		a.addEdge(0, 1);
+		a.addEdge(0, 2);
+		a.addEdge(0, 3);
+		a.addEdge(1, 4);
+		a.addEdge(2, 4);
+		a.addEdge(2, 5);
+		a.addEdge(4, 5);
+		
+		a.bfs(0);
+		
+		
+	}
+
+}
