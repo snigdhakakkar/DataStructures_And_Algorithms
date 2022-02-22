10005756d447043e5ee2730907ed9e6166ab9424
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-22 02:08:55 -0500

    This class contains definitions of basic concepts of graphs

---
+++ LeetCode/src/Graph/GraphBasicsAndTerminologies.java
@@ -0,0 +1,37 @@
+package Graph;
+
+public class GraphBasicsAndTerminologies {
+	
+	//Similar to a network of airports
+	
+	//Adjacency - if two nodes are adjacent to each other (neighbors)
+	
+	//Paths - e.g. A-F path : A - D - E - F; A-C Paths : A - B - C || A - D - B - C
+	
+	//Connected graph - a graph where you can reach from any node to any node
+	
+	//Non-connected graph: if we have a single node that is not connected to any node; then the graph is a non-connected one
+	
+	//Non-directed graph: where we don't have any directions i.e. you can't go from D to A but can go from A to D
+	
+	//Vertices: Airports or examples in our example - have certain properties such as names, people they can handle etc.
+	
+	//Edges: connection b/w two vertices
+	
+	//Adjacency matrix: definition of edges in the graph - if there is an edge b/w two vertices then it's represented by 1 else 0
+	
+	//Adjacency list: kind of 2-D array, for example array 0 comprising of A'connections would have B and D
+	
+	//Adjacency list can be represented as a linked list
+	
+	/*Different operations we can apply on a graph are as follows:
+	 * Breadth first search: first we see the neighbors of the vertices and then we go further
+	 * Depth first search: we first go in the deep level of one element then we move to the other element
+	 * Minimum spanning tree(MST): minimum number of edges so that I can cover all the nodes; Edges count = Vertices - 1
+	 * Topological sort
+	 * Weighted graphs: shorted path I can travel and I cover the shortest distance
+	 */
+	
+	
+
+}
