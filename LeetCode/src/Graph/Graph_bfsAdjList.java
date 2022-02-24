22c56a870090253cac8f76dc69e0db90f18ba560
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-24 02:45:23 -0500

    Added code to check if a directed graph has a cycle using BFS

--- LeetCode/src/Graph/Graph_bfsAdjList.java
+++ LeetCode/src/Graph/Graph_bfsAdjList.java
@@ -1,27 +1,39 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 public class Graph_bfsAdjList {
 	
 	List<List<Integer>> Graph_bfsAdjList;
+	HashMap<Integer, Integer> incomingDegree;
+	int visitedNodes;
+	int nodes;
+	Queue<Integer> sourceQueue;
 	boolean visited[];
 	
 	Graph_bfsAdjList(int nodes){
 		Graph_bfsAdjList = new ArrayList<>();
 		visited = new boolean[nodes];
+		incomingDegree = new HashMap<>();
+		sourceQueue = new LinkedList<>();
+		this.nodes = nodes;
+		visitedNodes = 0;
 		
 		for(int i = 0; i < nodes; i++) {
 			Graph_bfsAdjList.add(i, new ArrayList<>());
+			incomingDegree.put(i, 0);
 		}
 	}
 	
 	public void addEdge(int a, int b) {
 		Graph_bfsAdjList.get(a).add(b);
-		Graph_bfsAdjList.get(b).add(a); //if it is a directional graph, then this would get removed
+		//Graph_bfsAdjList.get(b).add(a); //if it is a directional graph, then this would get removed
+		incomingDegree.put(b, incomingDegree.get(b) + 1);
 	}
 	
 	public void bfs(int startIndex) {
@@ -43,6 +55,29 @@ public class Graph_bfsAdjList {
 				}
 			}
 		}
+	}
+	
+	public boolean ifDirectedGraphHasCycle() {
+		for(Map.Entry<Integer, Integer> entry : incomingDegree.entrySet()) {
+			if(entry.getValue() == 0) {
+				sourceQueue.add(entry.getKey());
+			}
+		}
+		
+		while(!sourceQueue.isEmpty()) {
+			visitedNodes++;
+			int source = sourceQueue.remove();
+			
+			List<Integer> childrenList = Graph_bfsAdjList.get(source);
+			for(Integer child : childrenList) {
+				incomingDegree.put(child, incomingDegree.get(child) - 1);
+				
+				if(incomingDegree.get(child) == 0) {
+					sourceQueue.add(child);
+				}
+			}
+		}
+		return nodes != visitedNodes;
 	}
 	
 	
