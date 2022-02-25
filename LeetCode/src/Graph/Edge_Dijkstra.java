ae8ec9282dcee069a6a545a9c5f8219629ece54c
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-25 03:43:02 -0500

    Code for edge being used in Dijkstra algorithm to compute the min distance in a directed graph between given source and destination

---
+++ LeetCode/src/Graph/Edge_Dijkstra.java
@@ -0,0 +1,12 @@
+package Graph;
+
+public class Edge_Dijkstra {
+	int targetNode;
+	int distanceFromNode;
+	
+	Edge_Dijkstra(int targetNode, int distance){
+		this.targetNode = targetNode;
+		this.distanceFromNode = distance;
+	}
+
+}
