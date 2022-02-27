a2f87fd076e84caebf9e7d9ab6fc33c77d7cb582
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-27 03:27:58 -0500

    Graph class for Q1_Route_between_nodes

---
+++ CrackingTheCode/src/Trees_And_Graphs/Graph.java
@@ -0,0 +1,23 @@
+package Trees_And_Graphs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Graph {
+	
+	private final Set<Node> nodes = new HashSet<Node>();
+	
+	public Set<Node> getNodes(){
+		return nodes;
+	}
+	
+	public void addNode(Node node) {
+		nodes.add(node);
+	}
+	
+	//returns whether or not a node was removed
+	public boolean removeNode(Node node) {
+		return nodes.remove(node);
+	}
+
+}
