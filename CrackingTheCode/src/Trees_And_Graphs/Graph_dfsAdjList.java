c234ba2000e4c1be2e56445b9a7d3deb643ea6f1
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-27 03:42:00 -0500

    Program for graph class for implementing Depth first search

---
+++ CrackingTheCode/src/Trees_And_Graphs/Graph_dfsAdjList.java
@@ -0,0 +1,56 @@
+package Trees_And_Graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Graph_dfsAdjList {
+		
+		List<List<Integer>> Graph_dfsAdjList;
+		boolean visited[];
+		int nodes;
+		
+		Graph_dfsAdjList(int nodes){
+			Graph_dfsAdjList = new ArrayList<>();
+			visited = new boolean[nodes];
+			this.nodes = nodes;
+			
+			for(int i = 0; i < nodes; i++) {
+				Graph_dfsAdjList.add(i, new ArrayList<>());
+			}
+		}
+		
+		public void addEdge(int a, int b) {
+			Graph_dfsAdjList.get(a).add(b);
+			//Graph_dfsAdjList.get(b).add(a); //if it is a directional graph, then this would get removed
+		}
+		
+		public void dfs(int start) {
+			Stack<Integer> stack = new Stack<>();
+			
+			stack.push(start);
+			visited[start] = true;
+			
+			while(!stack.isEmpty()) {
+				Integer node = stack.pop();
+				System.out.print(node + " ");
+				
+				List<Integer> neighborlist = Graph_dfsAdjList.get(node);
+				
+				for(Integer neighbor : neighborlist) {
+					if(!visited[neighbor]) {
+						stack.push(neighbor);
+						visited[neighbor] = true;
+					}
+				}
+			}
+			System.out.println();
+		}
+		
+		public boolean ifSourceConnectedToDestination(int source, int destination) {
+			dfs(source);
+			
+			return visited[destination];
+		}
+
+}
