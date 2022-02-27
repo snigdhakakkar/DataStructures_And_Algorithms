354a086ca85f9c2f847027046e6b3387b4e6b842
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-27 03:28:51 -0500

    Cracking the code interview Q1 (Chapter 4): Program to check if a route exists between two given nodes in a directed graph

---
+++ CrackingTheCode/src/Trees_And_Graphs/Q4_01_Route_Between_Nodes.java
@@ -0,0 +1,63 @@
+package Trees_And_Graphs;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Q4_01_Route_Between_Nodes {
+	
+	public static void main(String[] args) {
+		Graph g = new Graph();
+		
+		Node node1 = new Node(1);
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+		Node node4 = new Node(4);
+		Node node5 = new Node(5);
+		
+		node1.addAdjacent(node2);
+		node1.addAdjacent(node3);
+		node1.addAdjacent(node4);
+		node3.addAdjacent(node5);
+		
+		g.addNode(node1);
+		
+		System.out.println("Is Route exists : " + Q4_01_Route_Between_Nodes.search(g, node1, node5));
+	}
+	
+	public static boolean search(Graph g, Node start, Node end) {
+		if(start == end) {
+			return true;
+		}
+		
+		//using Linkedlist as a queue because we are conducting breadth-first search in this scenario
+		
+		LinkedList<Node> queue = new LinkedList<Node>();
+		start.visit();
+		queue.add(start);
+		
+		while(!queue.isEmpty()) {
+			Node current = queue.remove();
+			System.out.println("current node = " + current.data);
+			if(current.getAdjacent() != null) {
+				Iterator<Node> nodeIterator = current.getAdjacent().iterator();
+				
+				while(nodeIterator.hasNext()) {
+					Node neighbor = nodeIterator.next();
+					System.out.println("neighbor node = " + neighbor.data);
+					if(!neighbor.visited) {
+						if(neighbor == end) {
+							return true;
+						} else {
+							neighbor.visit();
+							queue.add(neighbor);
+						}
+					}
+				}
+					
+			}
+			current.visit();
+		}
+		return false;
+	}
+
+}
