607a857ea2b8af8f770ced48affc3e05dd184b9a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-27 03:42:26 -0500

    Program for checking if a route exists between a source and a destination using DFS

---
+++ CrackingTheCode/src/Trees_And_Graphs/Q4_01_Route_Between_Nodes_DFS.java
@@ -0,0 +1,25 @@
+package Trees_And_Graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Q4_01_Route_Between_Nodes_DFS {
+	
+	//time complexity: O(V+E); Space complexity: O(V)
+		public static void main(String[] args) {
+			int nodes = 5;
+			
+			Graph_dfsAdjList a = new Graph_dfsAdjList(nodes);
+			
+			a.addEdge(0, 1);
+			a.addEdge(1, 2);
+			a.addEdge(2, 0);
+			a.addEdge(3, 2);
+			a.addEdge(3, 0);
+			a.addEdge(2, 4);
+			
+			System.out.println(a.ifSourceConnectedToDestination(0,3));
+		}
+
+}
