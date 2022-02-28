71d453c0cccb4e392e39abd251b684b6025dde7f
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-28 05:39:31 -0500

    Cracking the code Q4 - Program to check whether a given binary tree is balanced or not
    Improved method of propagating errors if the tree is not balanced when checking height of each subtree

---
+++ CrackingTheCode/src/Trees_And_Graphs/Q4_04_CheckIfBalanced_Improved.java
@@ -0,0 +1,43 @@
+package Trees_And_Graphs;
+
+public class Q4_04_CheckIfBalanced_Improved {
+	//Time complexity: O(N), Space complexity: O(H)
+	public static boolean isBalanced(TreeNode root) {
+		return checkHeight(root) != Integer.MIN_VALUE;
+	}
+	
+	public static int checkHeight(TreeNode root) {
+		if (root == null) {
+			return -1;
+		}
+		
+		int leftHeight = checkHeight(root.left);
+		if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Propagate error up
+		
+		int rightHeight = checkHeight(root.right);
+		if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Propagate error up
+		
+		int heightDiff = leftHeight - rightHeight;
+		
+		if (Math.abs(heightDiff) > 1) {
+			return Integer.MIN_VALUE; // Found error -> pass it back
+		} 
+		else {
+			return Math.max(leftHeight, rightHeight) + 1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// Create balanced tree
+		int[] array = {0, 1, 2, 3, 5, 6, 7, 8, 9, 10};
+		TreeNode root = TreeNode.createMinimalBST(array);
+
+		
+		System.out.println("Is balanced? " + isBalanced(root));
+		
+		root.insertInOrder(4); // Add 4 to make it unbalanced
+
+		System.out.println("Is balanced? " + isBalanced(root));
+	}
+
+}
