28d961284f15db6fb0ab1049be6bbb01209eb61d
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-02-28 06:51:02 -0500

    Cracking the code - Q6 - Get the successor of each treenode

--- CrackingTheCode/src/Trees_And_Graphs/Q4_06_Successor.java
+++ CrackingTheCode/src/Trees_And_Graphs/Q4_06_Successor.java
@@ -1,5 +1,53 @@
 package Trees_And_Graphs;
 
 public class Q4_06_Successor {
+	
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		TreeNode root = TreeNode.createMinimalBST(array);
+		
+		for (int i = 0; i < array.length; i++) {
+			TreeNode node = root.find(array[i]);
+			TreeNode next = inorderSuccessor(node);
+			
+			if (next != null) {
+				System.out.println(node.data + "->" + next.data);
+			} else {
+				System.out.println(node.data + "->" + null);
+			}
+		}
+	}
+	
+	public static TreeNode inorderSuccessor(TreeNode n) {
+		if (n == null) {
+			return null;
+		}
+		
+		// Case1: Found right children -> return left most node of right subtree
+		if (n.parent == null || n.right != null) {
+			return leftMostChild(n.right); 
+		} else {
+			TreeNode q = n;
+			TreeNode x = q.parent;
+			// Case2: Go up until we're on left instead of right
+			while (x != null && x.left != q) {
+				q = x;
+				x = x.parent;
+			}
+			return x;
+		}
+	}
+	
+	public static TreeNode leftMostChild(TreeNode n) {
+		if (n == null) {
+			return null;
+		}
+		
+		while (n.left != null) {
+			n = n.left;
+		}
+		
+		return n;
+	}
 
 }
