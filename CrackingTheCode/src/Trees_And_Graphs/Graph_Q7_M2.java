d6955ad8f52e7f2b52d82d73b496d948b9b3e2dc
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-03-01 06:18:30 -0500

    Graph class for Q7 Cracking the code Method 2 - DFS

---
+++ CrackingTheCode/src/Trees_And_Graphs/Graph_Q7_M2.java
@@ -0,0 +1,30 @@
+package Trees_And_Graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph_Q7_M2 {
+	
+	private ArrayList<Project_Q7> nodes = new ArrayList<Project_Q7>();
+	private HashMap<String, Project_Q7> map = new HashMap<String, Project_Q7>();
+	
+	public Project_Q7 getOrCreateNode(String name) {
+		if (!map.containsKey(name)) {
+			Project_Q7 node = new Project_Q7(name);
+			nodes.add(node);
+			map.put(name, node);
+		}
+		return map.get(name);
+	}
+	
+	public void addEdge(String startName, String endName) {
+		Project_Q7 start = getOrCreateNode(startName);
+		Project_Q7 end = getOrCreateNode(endName);
+		start.addNeighbor(end);
+	}
+	
+	public ArrayList<Project_Q7> getNodes() {
+		return nodes;
+	}
+
+}
