5c1c50aa21703734ce64126daa07d54ecab41975
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-03-01 06:18:44 -0500

    Project class for Q7 Cracking the code Method 2 - DFS

---
+++ CrackingTheCode/src/Trees_And_Graphs/Project_Q7.java
@@ -0,0 +1,39 @@
+package Trees_And_Graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Project_Q7 {
+	public enum State {COMPLETE, PARTIAL, BLANK};
+	private ArrayList<Project_Q7> children = new ArrayList<Project_Q7>();
+	private HashMap<String, Project_Q7> map = new HashMap<String, Project_Q7>();
+	private String name;
+	private State state = State.BLANK;
+	
+	public Project_Q7(String n) {
+		name = n;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void addNeighbor(Project_Q7 node) {
+		if (!map.containsKey(node.getName())) {
+			children.add(node);
+			map.put(node.getName(), node);
+		}
+	}
+	
+	public ArrayList<Project_Q7> getChildren() {
+		return children;
+	}
+	
+	public State getState() { 
+		return state;
+	}
+	
+	public void setState(State st) {
+		state = st;
+	}
+}
