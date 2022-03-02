b4203cf651163d172caf0be129d0e231af7e986a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-03-02 04:54:38 -0500

    Q10 - Chapter 4 (CTCI) Optimized method

---
+++ CrackingTheCode/src/Trees_And_Graphs/Q4_10_Check_Subtree_Method2.java
@@ -0,0 +1,101 @@
+package Trees_And_Graphs;
+
+public class Q4_10_Check_Subtree_Method2 {
+	
+	//time complexity: O(N + KM) where K is the number of occurrences of root node of t2 in t1 i.e. each time when matchTree is called
+	
+	public static boolean containsTree(TreeNode t1, TreeNode t2) {
+		if (t2 == null) {
+			return true; // The empty tree is a subtree of every tree.
+		}
+		return subTree(t1, t2);
+	}
+	
+	/* Checks if the binary tree rooted at r1 contains the binary tree 
+	 * rooted at r2 as a subtree somewhere within it.
+	 */
+	public static boolean subTree(TreeNode r1, TreeNode r2) {
+		if (r1 == null) {
+			return false; // big tree empty & subtree still not found.
+		} else if (r1.data == r2.data && matchTree(r1,r2)) {
+			return true;
+		}
+		return subTree(r1.left, r2) || subTree(r1.right, r2);
+	}
+	
+	/* Checks if the binary tree rooted at r1 contains the 
+	 * binary tree rooted at r2 as a subtree starting at r2.
+	 */
+	
+	public static boolean matchTree(TreeNode r1, TreeNode r2) {
+		if (r1 == null && r2 == null) {
+			return true; // nothing left in the subtree
+		} else if (r1 == null || r2 == null) { 
+			return false; // exactly one tree is empty, therefore trees don't match
+		}  else if (r1.data != r2.data) {  
+			return false;  // data doesn't match
+		} else {
+			return matchTree(r1.left, r2.left) && matchTree(r1.right, r2.right);
+		}
+	}
+	
+	public static void main(String[] args) {
+		// t2 is a subtree of t1
+		int[] array1 = {1, 2, 1, 3, 1, 1, 5};
+		int[] array2 = {2, 3, 1};
+				
+		TreeNode t1 = createTreeFromArray(array1);
+		TreeNode t2 = createTreeFromArray(array2);
+				
+		if (containsTree(t1, t2)) {
+			System.out.println("t2 is a subtree of t1");
+		} else {
+			System.out.println("t2 is not a subtree of t1");
+		}
+		
+		// t4 is not a subtree of t3
+		int[] array3 = {1, 2, 3};
+		
+		TreeNode t3 = createTreeFromArray(array1);
+		TreeNode t4 = createTreeFromArray(array3);
+		
+		if (containsTree(t3, t4)) {
+			System.out.println("t4 is a subtree of t3");
+		} else {
+			System.out.println("t4 is not a subtree of t3");
+		}		
+	}
+	
+	/* Creates tree by mapping the array left to right, top to bottom. */
+	public static TreeNode createTreeFromArray(int[] array) {
+		if (array.length > 0) {
+			TreeNode root = new TreeNode(array[0]);
+			java.util.Queue<TreeNode> queue = new java.util.LinkedList<TreeNode>();
+			queue.add(root);
+			boolean done = false;
+			int i = 1;
+			while (!done) {
+				TreeNode r = (TreeNode) queue.element();
+				if (r.left == null) {
+					r.left = new TreeNode(array[i]);
+					i++;
+					queue.add(r.left);
+				} else if (r.right == null) {
+					r.right = new TreeNode(array[i]);
+					i++;
+					queue.add(r.right);
+				} else {
+					queue.remove();
+				}
+				if (i == array.length) {
+					done = true;
+				}
+			}
+			return root;
+		} else {
+			return null;
+		}
+	}
+
+
+}
