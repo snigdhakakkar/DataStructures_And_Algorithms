693fc9da50cf53ccafb6af40856f90155a30da83
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-03-04 06:00:35 -0500

    check if the given number is a palindrome

---
+++ LeetCode/src/BasicCodingProblems/isPalindrome.java
@@ -0,0 +1,23 @@
+package BasicCodingProblems;
+
+public class isPalindrome {
+	//time complexity: O(N) where N is the number of digits
+	public static void main(String[] args) {
+		isPalindrome a = new isPalindrome();
+		System.out.println(a.checkIfPalindrome(12321));
+		System.out.println(a.checkIfPalindrome(123));
+	}
+	
+	public boolean checkIfPalindrome(int n) {
+		int s = 0;
+		int t = n;
+		
+		while(t > 0) {
+			s = s * 10 + t % 10;
+			t = t / 10;
+		}
+		
+		return s == n;
+	}
+
+}
