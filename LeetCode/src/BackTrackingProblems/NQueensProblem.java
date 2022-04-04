f7ffbae097147c9f675c0e5039a70bd08299d6ae
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-04 05:58:23 -0400

    NQueens Problem - BackTracking

--- LeetCode/src/BackTrackingProblems/NQueensProblem.java
+++ LeetCode/src/BackTrackingProblems/NQueensProblem.java
@@ -2,6 +2,13 @@ package BackTrackingProblems;
 
 public class NQueensProblem {
 	
+	final int N;
+	
+	NQueensProblem(int N){
+		this.N = N;
+	}
+	
+	
 	//we are given number N
 	//we have to place these N queens in NXN matrix
 	//First condition: single queen in one row
@@ -9,6 +16,12 @@ public class NQueensProblem {
 	//Third condition: single queen in diagonal
 	
 	public static void main(String[] args) {
+		int n = 4; //queencount
+		NQueensProblem prob = new NQueensProblem(n);
+		prob.queenProblem();
+	}
+	
+	public boolean queenProblem() {
 		
 	}
 
