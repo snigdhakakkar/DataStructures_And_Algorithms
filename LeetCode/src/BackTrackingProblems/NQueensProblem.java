20b86bd1871ce93a0cf8efc93eefec391488aa87
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-04 06:20:26 -0400

    NQueens Problem - BackTracking Problems

--- LeetCode/src/BackTrackingProblems/NQueensProblem.java
+++ LeetCode/src/BackTrackingProblems/NQueensProblem.java
@@ -80,5 +80,14 @@ public class NQueensProblem {
 		
 		return true;
 	}
+	
+	private void printQueenPlacement(int[][] mat) {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 
 }
