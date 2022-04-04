b3fc69835957f717e41c08549ca9a5e7c225abf0
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-04 05:45:53 -0400

    Conditions to be met for NQueens problem

---
+++ LeetCode/src/BackTrackingProblems/NQueensProblem.java
@@ -0,0 +1,15 @@
+package BackTrackingProblems;
+
+public class NQueensProblem {
+	
+	//we are given number N
+	//we have to place these N queens in NXN matrix
+	//First condition: single queen in one row
+	//Second condition: single queen in one column
+	//Third condition: single queen in diagonal
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
