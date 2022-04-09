4444e80f7114730385b53033747d54dac05ede54
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-09 18:30:50 -0400

    Code updated

--- CrackingTheCode/src/array/String_permutations_check.java
+++ CrackingTheCode/src/array/String_permutations_check.java
@@ -8,7 +8,7 @@ public class String_permutations_check {
 	//'God' is not a permutation of 'dog'
 	//'god    ' is not a permutation of 'dog'
 	
-	       
+	         
 	public static String sort(String s) {
 		char[] content = s.toCharArray();
 		Arrays.sort(content);
