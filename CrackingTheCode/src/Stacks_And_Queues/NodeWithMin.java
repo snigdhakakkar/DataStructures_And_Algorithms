4414b9cf29d7cbae0fbdc6cd99c49dcc06e5f286
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-14 05:37:53 -0400

    Node with minimum value stored for a stack

---
+++ CrackingTheCode/src/Stacks_And_Queues/NodeWithMin.java
@@ -0,0 +1,12 @@
+package Stacks_And_Queues;
+
+public class NodeWithMin {
+	
+	public int value;
+    public int min;
+    public NodeWithMin(int v, int min){
+        this.value = v;
+        this.min = min;
+    }
+
+}
