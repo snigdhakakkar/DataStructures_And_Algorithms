d12527e26ced5416114129730ee09e49b0ded970
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-22 05:23:58 -0400

    Q6 Approach 1 Conversion

---
+++ CrackingTheCode/src/bitManipulation/Q6ConversionApproach1.java
@@ -0,0 +1,35 @@
+package bitManipulation;
+
+public class Q6ConversionApproach1 {
+	
+	public static int bitSwapRequired(int a, int b) {
+		int count = 0;
+		int c = a ^ b;
+		while (c != 0) {
+			count += c & 1; // Increment count if c ends with a 1
+			c >>>= 1; // Shift right by 1
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		int a = -23432;
+		int b = 512132;
+		System.out.println(a + ": " + toFullBinaryString(a));
+		System.out.println(b + ": " + toFullBinaryString(b));
+		System.out.println("Required number of bits: " + bitSwapRequired(a, b));
+	}
+	
+	public static String toFullBinaryString(int a) {
+		String s = "";
+		for (int i = 0; i < 32; i++) {
+			Integer lsb = new Integer(a & 1);
+			s = lsb.toString() + s;
+			a = a >> 1;
+		}
+		return s;
+	}
+	
+	
+
+}
