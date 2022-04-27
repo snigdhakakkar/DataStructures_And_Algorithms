43883b0e67da3a8b3cf027ddb8a7a11bead5355a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-27 04:16:07 -0400

    Blue-eyed island 6.6

---
+++ CrackingTheCode/src/MathAndLogicPuzzles/BlueEyedPersons.java
@@ -0,0 +1,18 @@
+package MathAndLogicPuzzles;
+
+public class BlueEyedPersons {
+	
+	/*
+	 * Let us apply the base case and build approach. Assume there were n people on the island out of which c people were blue eyed. We are explicitly told that c > 0
+	 * Case c = 1: Exactly one person is blue eyed.
+	 * Assumign that all the people are intelligent, the blue-eyed person should look around and see that no one has blue eyes. Since no one but at least one person has blue eyes,
+	 * he must conclude that it is him who has blue eyes and must leave the island in the evening flight.
+	 * 
+	 * Case c = 2, Exactly 2 people have blue eyes
+	 * The two blue-eyes persons would ee each other but would be unsure if c = 1 or 2.
+	 * They know from the previous case that if c = 1, the blue eyed person would leave the island the same evening.
+	 * Therefore, if the other blue-eyed person is there, he must deduce c= 2, which means that he himself has blue eyes.
+	 * Both men would then leave on the flight on second night.
+	 */
+
+}
