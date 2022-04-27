6f615f133ab236f7f6a9c1dee791b8bd3f1947e4
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-27 04:40:54 -0400

    Added the code for 6.7

--- CrackingTheCode/src/MathAndLogicPuzzles/TheApocalypse.java
+++ CrackingTheCode/src/MathAndLogicPuzzles/TheApocalypse.java
@@ -1,5 +1,7 @@
 package MathAndLogicPuzzles;
 
+import java.util.Random;
+
 public class TheApocalypse {
 	
 	/*
@@ -25,4 +27,37 @@ public class TheApocalypse {
 	 * There would be 50% boys and 50% girls on average
 	 */
 
+	
+	public static int[] runOneFamily() {
+		Random random = new Random();
+		int boys = 0;
+		int girls = 0;
+		while (girls == 0) { // until we have a girl
+			if (random.nextBoolean()) { // girl
+				girls += 1;
+			} else { // boy
+				boys += 1;
+			}
+		}
+		
+		int[] genders = {girls, boys};
+			return genders;
+	}
+		
+	public static double runNFamilies(int n) {
+		int boys = 0;
+		int girls = 0;
+		for (int i = 0; i < n; i++) {
+			int[] genders = runOneFamily();
+			girls += genders[0];
+			boys += genders[1];
+			}
+		return girls / (double) (boys + girls); 
+		}
+		
+	public static void main(String[] args) {
+		double ratio = runNFamilies(10000000);
+		System.out.println(ratio);
+
+		}
 }
