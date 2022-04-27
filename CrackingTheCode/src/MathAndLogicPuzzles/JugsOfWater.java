c18a1381ff92495ac858c08ec87e6ee7d79fc6f9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-27 04:09:08 -0400

    Jugs of water 6.5

---
+++ CrackingTheCode/src/MathAndLogicPuzzles/JugsOfWater.java
@@ -0,0 +1,15 @@
+package MathAndLogicPuzzles;
+
+public class JugsOfWater {
+	
+	/*
+	 * If the two jugs are relatively prime sized, one can measure any value between one and the sum of the jug sizes.
+	 * Suppose, we initially fill 5 Quart completely and 0 in 3 Quart jug
+	 * Then, we move the contents to 3 Quart jug - 2,3 Quart in 5 Quart and 3 Quart jug respectively.
+	 * Then, we emptyt he contents of 3 Quart jug as we dump it.
+	 * SO, we move the remaining 2 quart content of jug 5 to jug 3.
+	 * Now, we can fill 5 Quart more in 5 Quart jug. Then, we can move 1 quart to the 3 quart jug as it already has 2 quarts in it.
+	 * The remaining water in 5 Quart jug would be 4 quarts.
+	 */
+
+}
