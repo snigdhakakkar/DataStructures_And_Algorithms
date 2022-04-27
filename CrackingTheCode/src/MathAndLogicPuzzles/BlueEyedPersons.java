269af106bf2b646c019ce29d02a465edc24e8f4e
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-04-27 04:19:21 -0400

    Added general case for 6.6

--- CrackingTheCode/src/MathAndLogicPuzzles/BlueEyedPersons.java
+++ CrackingTheCode/src/MathAndLogicPuzzles/BlueEyedPersons.java
@@ -13,6 +13,13 @@ public class BlueEyedPersons {
 	 * They know from the previous case that if c = 1, the blue eyed person would leave the island the same evening.
 	 * Therefore, if the other blue-eyed person is there, he must deduce c= 2, which means that he himself has blue eyes.
 	 * Both men would then leave on the flight on second night.
+	 * 
+	 * Case c>2: General case
+	 * As we increase c, the same logic continues.
+	 * If c = 3, those three people will immediately know that there are 2 or 3 people with ble eyes.
+	 * When there were just 2 people, they would have left on the second night. 
+	 * So, when the others are still around on the secodn night, each person would deduce that c=3 and he himself has blue eyes and will leave the island next evening
+	 * This same pattern continues for any values of c. If c men have blue eyes, it will take c nights to leave the island.
 	 */
 
 }
