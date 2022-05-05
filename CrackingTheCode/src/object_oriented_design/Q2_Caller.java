a9bd4325a459851395d5353c8357d3ea7c75f9fa
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-05 05:29:27 -0400

    Caller class

---
+++ CrackingTheCode/src/object_oriented_design/Q2_Caller.java
@@ -0,0 +1,12 @@
+package object_oriented_design;
+
+public class Q2_Caller {
+	
+	private String name;
+	private int userId;
+	public Q2_Caller(int id, String nm) {
+		name = nm;
+		userId = id;
+	}
+
+}
