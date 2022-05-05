2b60be5116d210604ba17219c925b5f2c3f79de7
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-05 05:49:32 -0400

    Q2 Test class

---
+++ CrackingTheCode/src/object_oriented_design/Question2.java
@@ -0,0 +1,13 @@
+package object_oriented_design;
+
+public class Question2 {
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Q2_CallHandler ch = new Q2_CallHandler();
+	}
+
+}
