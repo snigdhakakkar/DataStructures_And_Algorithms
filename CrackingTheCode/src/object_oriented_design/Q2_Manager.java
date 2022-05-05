ba0bf897c85a7283b013f0b5f3d34fbeb648d8ac
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-05 05:27:53 -0400

    manager class which extends the eployee class

---
+++ CrackingTheCode/src/object_oriented_design/Q2_Manager.java
@@ -0,0 +1,10 @@
+package object_oriented_design;
+
+public class Q2_Manager extends Q2_Employee {
+	
+	 public Q2_Manager(Q2_CallHandler callHandler) {
+	    	super(callHandler);
+	    	rank = Q2_Rank.Q2_Manager;
+	    }
+
+}
