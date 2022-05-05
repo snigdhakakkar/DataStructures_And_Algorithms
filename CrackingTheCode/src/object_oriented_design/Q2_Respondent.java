44e72fb3bc167e9077d5a5c0e050444373faba3d
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-05 06:01:30 -0400

    made a few modifications

--- CrackingTheCode/src/object_oriented_design/Q2_Respondent.java
+++
@@ -1,9 +0,0 @@
-package object_oriented_design;
-
-public class Q2_Respondent extends Q2_Employee{
-	
-	public Q2_Respondent(Q2_CallHandler callHandler) {
-    	super(callHandler);
-    	rank = Q2_Rank.Q2_Responder;
-    }
-}
