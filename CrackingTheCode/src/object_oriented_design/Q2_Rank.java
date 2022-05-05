a61ec987cb25acc228d62f5986b6e219142ea77d
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-05 05:27:01 -0400

    rank Enum for Q2

---
+++ CrackingTheCode/src/object_oriented_design/Q2_Rank.java
@@ -0,0 +1,19 @@
+package object_oriented_design;
+
+public enum Q2_Rank {
+	
+	Q2_Responder (0),
+	Q2_Manager (1),
+	Q2_Director (2);
+	
+	private int value;
+	
+	private Q2_Rank(int v) {
+		value = v;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+}
