de0a9ad4f5ca469afcdc77c048ed4e8db0faafda
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-10 05:44:23 -0400

    Message class prob 7.7

---
+++ CrackingTheCode/src/object_oriented_design/ChatServer/Message.java
@@ -0,0 +1,22 @@
+package object_oriented_design.ChatServer;
+
+import java.util.Date;
+
+public class Message {
+	
+	private String content;
+	private Date date;
+	public Message(String content, Date date) {
+		this.content = content;
+		this.date = date;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+}
