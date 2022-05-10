53b7aaff19e13037b5a9d5525764ff2cf064735a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-10 04:31:59 -0400

    Motorcycle class prob 7.4

---
+++ CrackingTheCode/src/object_oriented_design/ParkingLot/Motorcycle.java
@@ -0,0 +1,18 @@
+package object_oriented_design.ParkingLot;
+
+public class Motorcycle extends Vehicle {
+	
+	public Motorcycle() {
+		spotsNeeded = 1;
+		size = VehicleSize.Motorcycle;
+	}
+	
+	public boolean canFitInSpot(ParkingSpot spot) {
+		return true;
+	}
+	
+	public void print() {
+		System.out.print("M");
+	}	
+
+}
