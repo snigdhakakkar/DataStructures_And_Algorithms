2799cf73d074e0d4af5048a5e7020b67ea24353a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-10 06:01:28 -0400

    PrivateChat class prob 7.7

---
+++ CrackingTheCode/src/object_oriented_design/ChatServer/PrivateChat.java
@@ -0,0 +1,17 @@
+package object_oriented_design.ChatServer;
+
+public class PrivateChat extends Conversation {
+	public PrivateChat(User user1, User user2) {
+		participants.add(user1);
+		participants.add(user2);
+	}
+	
+	public User getOtherParticipant(User primary) {
+		if (participants.get(0) == primary) {
+			return participants.get(1);
+		} else if (participants.get(1) == primary) {
+			return participants.get(0);
+		}
+		return null;
+	}
+}
