ffedbfc5bd96cf618e772adb7a743613b6a79468
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-11 04:56:15 -0400

    Circular Array main class Prob 7.9

---
+++ CrackingTheCode/src/object_oriented_design/CircularArray/Question.java
@@ -0,0 +1,26 @@
+package object_oriented_design.CircularArray;
+
+public class Question {
+	
+	public static void main(String[] args) {
+		int size = 10;
+		CircularArray<String> array = new CircularArray<String>(size);
+		for (int i = 0; i < size; i++) {
+			array.set(i, String.valueOf(i));
+		}
+		
+		array.rotate(3);
+		for (int i = 0; i < size; i++) {
+			System.out.println(array.get(i));
+		}
+		
+		System.out.println("");
+		
+		array.rotate(2);
+		for (String s : array) {
+			System.out.println(s);
+		}
+	}
+
+
+}
