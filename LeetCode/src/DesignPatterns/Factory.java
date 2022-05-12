f5857e8eac88fd4ab72eb0853c4fdcae38f045a3
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-12 04:45:52 -0400

    Factory design pattern

---
+++ LeetCode/src/DesignPatterns/Factory.java
@@ -0,0 +1,19 @@
+package DesignPatterns;
+
+/*
+ * widely used in code base
+ * Creational design pattern - related to the creation of object
+ * Factory pattern is used in case when a class is a super class and we have multiple sub-classes; we want to return the instance based on input
+ * provides abstraction between implementation & client classes
+ * remove the instantation of client classes from client code
+ * 
+ * superclass can be an abstract class / an interface / a basic java class
+ * factory class has a static method which returns the instance of a sub-class based on the input
+ * 
+ * 
+ * 
+ */
+
+public class Factory {
+
+}
