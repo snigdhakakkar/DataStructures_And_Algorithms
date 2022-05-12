08ba10983b5657cff16faf30ff195a73d63aec82
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-12 04:15:10 -0400

    Question class prob 7.10

---
+++ CrackingTheCode/src/object_oriented_design/Minesweeper/Question.java
@@ -0,0 +1,10 @@
+package object_oriented_design.Minesweeper;
+
+public class Question {
+	
+	public static void main(String[] args) {
+		Game game = new Game(7, 7, 3);
+		game.initialize();
+		game.start();
+	}
+}
