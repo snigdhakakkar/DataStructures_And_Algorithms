c9daf5aed31c55b07b951ceacd9585f48f5959d1
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-13 16:45:52 -0400

    Search a given target element in a rotated sorted array.

---
+++ LeetCode/src/Top75/Search_In_Rotated_SubArray_Approach2.java
@@ -0,0 +1,35 @@
+package Top75;
+
+public class Search_In_Rotated_SubArray_Approach2 {
+	
+	//time complexity: O(logN), Space complexity: O(1)
+    //One-pass binary search
+    
+    public int search(int[] nums, int target){
+        int start = 0, end = nums.length - 1;
+        
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            
+            if(nums[mid] == target){
+                return mid;
+                //if the middle number is in the unrotated segment and target value is less than the mid value
+            } else if (nums[mid] >= nums[start]){
+                if (target >= nums[start] && target < nums[mid]){
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target <= nums[end] && target > nums[mid]){
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+
+}
