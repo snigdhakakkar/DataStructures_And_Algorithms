82731aa838ec83dc068f3696780a93132a5031a0
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-18 11:50:33 -0400

    Check if we can partition an array into two subsets with equal sum - OPTIMIZEDP - 1D Array approach

--- LeetCode/src/Top75/partition_equal_subset_sum.java
+++ LeetCode/src/Top75/partition_equal_subset_sum.java
@@ -109,5 +109,31 @@ public class partition_equal_subset_sum {
 	        
 	       
 	    }
+	    
+	    public boolean canPartitionIV(int[] nums) {
+	        //approach 4: Optimized DP - Using 1D Array
+	        //time complexity: O(mn) ; space complexity: O(m)
+	        
+	        if (nums.length == 0)
+	            return false;
+	        int totalSum = 0;
+	        // find sum of all array elements
+	        for (int num : nums) {
+	            totalSum += num;
+	        }
+	        
+	        // if totalSum is odd, it cannot be partitioned into equal sum subset
+	        if (totalSum % 2 != 0) return false;
+	        int subSetSum = totalSum / 2;
+	        boolean dp[] = new boolean[subSetSum + 1];
+	        dp[0] = true;
+	        
+	         for (int curr : nums) {
+	              for (int j = subSetSum; j >= curr; j--){
+	                  dp[j] |= dp[j - curr];
+	              }
+	         }
+	       return dp[subSetSum];
+	    }
 
 }
