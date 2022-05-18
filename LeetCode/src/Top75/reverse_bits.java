c213604cfeb7665148e80c3308950b63eddb9598
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-18 16:19:28 -0400

    reverse bits in a 32-bit

--- LeetCode/src/Top75/reverse_bits.java
+++ LeetCode/src/Top75/reverse_bits.java
@@ -32,6 +32,48 @@ public class reverse_bits {
         
         return ret;
     }
+    
+//Byte by byte with memoization
+    
+    /* The algorithm is documented as "reverse the bits in a byte with 3 operations" on the online book called Bit Twiddling Hacks by Sean Eron Anderson, where one can find more details.
+    
+    Algorithm
+
+We iterate over the bytes of an integer. To retrieve the right-most byte in an integer, again we apply the bit AND operation (i.e. n & 0xff) with the bit mask of 11111111. 
+
+For each byte, first we reverse the bits within the byte, via a function called reverseByte(byte). Then we shift the reversed bits to their final positions. 
+
+With the function reverseByte(byte), we apply the technique of memoization, which caches the result of the function and returns the result directly for the future invocations of the same input.
+
+def reverseByte(byte):
+    return (byte * 0x0202020202 & 0x010884422010) % 1023
+    
+    */
+    
+    
+    /* Time Complexity: O(1).; space complexity: O(1)
+    */
+    
+    public int reverseByte(int byte, Map<int, int> cache) {
+        if (cache.find(byte) != cache.end()) {
+            return cache[byte];
+        }
+        
+        int value = (byte * 0x0202020202 & 0x010884422010) % 1023;
+        cache.emplace(byte, value);
+        return value;
+    }
+    
+    public int reverseBitsI(int n){
+        int ret = 0, power = 24;
+        Map<int, int> cache;
+         while (n != 0) {
+            ret += reverseByte(n & 0xff, cache) << power;
+            n = n >> 8;
+            power -= 8;
+        }
+        return ret;
+    }
 	
 	
 
