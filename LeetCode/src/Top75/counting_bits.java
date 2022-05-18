ac5215eb648e38fd057790624a0c59cc85f8cfc9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-18 14:16:43 -0400

    Counting the bits for each x until n number

--- LeetCode/src/Top75/counting_bits.java
+++ LeetCode/src/Top75/counting_bits.java
@@ -49,5 +49,15 @@ public class counting_bits {
         }
         return ans;
     }
+    
+    public int[] countBitsII(int n) {
+        int[] ans = new int[n + 1];
+        for (int x = 1; x <= n; ++x) {
+            // x / 2 is x >> 1 and x % 2 is x & 1
+            ans[x] = ans[x >> 1] + (x & 1); 
+        }
+        return ans;
+    }
+        
 
 }
