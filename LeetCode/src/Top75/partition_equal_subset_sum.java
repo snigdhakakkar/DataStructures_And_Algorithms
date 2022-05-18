849a7a31e7594dd42050c2b85dfc44d4b6a433cf
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-18 11:11:04 -0400

    Check if we can partition an array into two subsets with equal sum - recursion with memoization approach

--- LeetCode/src/Top75/partition_equal_subset_sum.java
+++ LeetCode/src/Top75/partition_equal_subset_sum.java
@@ -21,7 +21,7 @@ public class partition_equal_subset_sum {
 	        //recursively checking whether there exists a subset with target sum or not
 	    }
 	    
-	    private boolean recursion(int[] nums, int index, int sum) {
+	 private boolean recursion(int[] nums, int index, int sum) {
 	        //base cases
 	        if(sum == 0) { 
 	        return true;
@@ -34,6 +34,43 @@ public class partition_equal_subset_sum {
 	        //either choose nums[index] or not
 	    }
 	    
+	    private Boolean[][] memo; //use memo to store the result of subproblems
 	    
+	    public boolean canPartitionI(int[] nums) {
+	        
+	         //Approach 2: DP - Recursion with memoization approach
+	        // we divide the original problem into several sub problems and save same sub problems (memo)
+	        //time complexity: O(mn), space complexity: O(mn) where m is the half of the sum of input array
+	        
+	        int sum = 0;
+	        for(int num : nums) {
+	            sum += num;
+	        }
+	        if (sum % 2 != 0) {
+	            return false;
+	        }
+	        
+	        sum /= 2;
+	        memo = new Boolean[nums.length + 1][sum + 1];
+	        return recursion(nums, 0, sum);
+	    }
+	    
+	    private boolean recursionI(int[] nums, int index, int sum){
+	        if(sum == 0){
+	            return true;
+	        }
+	        
+	        if (sum < 0 || index == nums.length) {
+	            return false;
+	        }
+	        
+	        if (memo[index][sum] != null) {
+	            return memo[index][sum];
+	        }
+	        
+	        boolean res = recursion(nums, index + 1, sum - nums[index]) || recursion(nums, index + 1, sum);
+	        memo[index][sum] = res;
+	        return res;
+	    }
 
 }
