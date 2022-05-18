da1670a8ed243d205302cec33685304d7fdc69db
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-18 11:43:06 -0400

    Check if we can partition an array into two subsets with equal sum - DP Bottom up approach

--- LeetCode/src/Top75/partition_equal_subset_sum.java
+++ LeetCode/src/Top75/partition_equal_subset_sum.java
@@ -72,5 +72,42 @@ public class partition_equal_subset_sum {
 	        memo[index][sum] = res;
 	        return res;
 	    }
+	    
+	    public boolean canPartitionII(int[] nums) {
+	        // problem has Overlapping Subproblem property and can be solved using Dynamic Programming.
+	         //Approach 3: DP  approach - bottom up approach
+	        
+	        //time complexity: O(m.n) where m is the subsetsum and n is the number of array elements; space complexity: O(mn)
+	        
+	        int totalSum = 0;
+	        //find the sum of all the array elements
+	        for (int num : nums) {
+	            totalSum += num;
+	        }
+	        
+	        //if totalSum is odd, it cannot be partitioned into equal sum subset
+	        if (totalSum % 2 != 0 ){
+	            return false;
+	        }
+	        
+	        int subSetSum = totalSum / 2;
+	        int n = nums.length;
+	        boolean dp[][] = new boolean[n + 1][subSetSum + 1];
+	        dp[0][0] = true;
+	        for (int i = 1; i <= n; i++) {
+	            int curr = nums[i - 1];
+	            for (int j = 0; j <= subSetSum; j++){
+	                if (j < curr){
+	                    dp[i][j] = dp[i - 1][j];
+	                } else {
+	                    dp[i][j] = dp[i - 1][j] || (dp[i - 1][j - curr]);
+	                }
+	            }
+	        }
+	        
+	        return dp[n][subSetSum];
+	        
+	       
+	    }
 
 }
