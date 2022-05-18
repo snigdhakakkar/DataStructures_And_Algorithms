2958906e4595fe047a7842211b1c335231981d43
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-18 16:38:39 -0400

    no. of ways to climb n steps in a staircase

--- LeetCode/src/Top75/climbing_stairs.java
+++ LeetCode/src/Top75/climbing_stairs.java
@@ -32,5 +32,32 @@ public class climbing_stairs {
         
         return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
     }
+    
+ public int climbStairsI(int n) {
+        
+        //Approach 2 - recursion and memoization approach
+        
+        //Time complexity : O(n); space complexity: O(n)
+        int memo[] = new int[n + 1];
+        
+        return climb_StairsI(0, n, memo);
+    }
+    
+    public int climb_StairsI(int i, int n, int memo[]){
+        if (i > n) {
+            return 0;
+        }
+        
+        if (i == n) {
+            return 1;
+        }
+        
+         if (memo[i] > 0) {
+            return memo[i];
+        }
+        
+        memo[i] =  climb_StairsI(i + 1, n, memo) + climb_StairsI(i + 2, n, memo);
+        return memo[i];
+    }
 
 }
