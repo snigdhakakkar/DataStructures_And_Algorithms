50a87421988140040ffa2ce2df2221365d7c9790
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-19 15:30:18 -0400

    find the longest common subsequence - IMPROVED MEMOIZATION APPROACH

--- LeetCode/src/Top75/longest_common_subsequence.java
+++ LeetCode/src/Top75/longest_common_subsequence.java
@@ -84,5 +84,48 @@ public class longest_common_subsequence {
         
         return memo[p1][p2];
     }
+    
+    public int longestCommonSubsequenceI(String text1, String text2) {
+        //Approach 2 - improved memoization
+        // Make the memo big enough to hold the cases where the pointers go over the edges of the strings.
+        this.memo = new int[text1.length() + 1][text2.length() + 1];
+        
+        // We need to initialise the memo array to -1's so that we know
+        // whether or not a value has been filled in. Keep the base cases
+        // as 0's to simplify the later code a bit.
+        
+        for (int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                this.memo[i][j] = -1;
+            }
+        }
+        
+        this.text1 = text1;
+        this.text2 = text2;
+        return memoSolveI(0, 0);
+          
+    }
+    
+    private int memoSolveI(int p1, int p2) {
+        // Check whether or not we've already solved this subproblem.
+        // This also covers the base cases where p1 == text1.length
+        // or p2 == text2.length.
+        
+        if (memo[p1][p2] != -1) {
+            return memo[p1][p2];
+        }
+        
+        //recursive cases
+        int answer = 0;
+        if (text1.charAt(p1) == text2.charAt(p2)) {
+            answer = 1 + memoSolve(p1 + 1, p2 + 1);
+        } else {
+            answer = Math.max(memoSolve(p1, p2 + 1), memoSolve(p1 + 1, p2));
+        }
+        
+        // Add the best answer to the memo before returning it.
+        memo[p1][p2] = answer;
+        return memo[p1][p2];
+    }
 
 }
