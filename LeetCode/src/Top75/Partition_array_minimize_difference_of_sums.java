75e331139785cd31d9bf989ee18fc63bc66cf6c7
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-19 10:41:40 -0400

    yet to complete the code

---
+++ LeetCode/src/Top75/Partition_array_minimize_difference_of_sums.java
@@ -0,0 +1,17 @@
+package Top75;
+
+/*
+ * Problem statement - Divide the array into two parts, such that the 
+ * difference between the sums of these two parts is minimized and the numbers 
+ * of elements in these two parts differ by at most 1.
+
+Constraints:
+Number of TestCases: 50
+Size of array: 1 <= n <= 200. Hint – use dp 
+ */
+
+public class Partition_array_minimize_difference_of_sums {
+	
+	
+
+}
