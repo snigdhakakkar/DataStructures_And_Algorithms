8f95e0f0c140526058cc546af1f6f91b07a052ef
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-19 16:12:13 -0400

    find the longest common subsequence - dynamic programming problem

--- LeetCode/src/Top75/longest_common_subsequence.java
+++ LeetCode/src/Top75/longest_common_subsequence.java
@@ -128,5 +128,27 @@ public class longest_common_subsequence {
         memo[p1][p2] = answer;
         return memo[p1][p2];
     }
+    
+    public int longestCommonSubsequenceII(String text1, String text2) {
+        //Approach 3 - dynamic programming
+       // Make a grid of 0's with text2.length() + 1 columns 
+    // and text1.length() + 1 rows.
+        int[][] dpGrid = new int[text1.length() + 1][text2.length() + 1];
+        // Iterate up each column, starting from the last one.
+        for (int col = text2.length() - 1; col >= 0; col--) {
+            for (int row = text1.length() - 1; row >= 0; row--){
+                // If the corresponding characters for this cell are the same...
+                if (text1.charAt(row) == text2.charAt(col)){
+                    dpGrid[row][col] = 1 + dpGrid[row + 1][col + 1];
+        // Otherwise they must be different...
+                } else {
+                    dpGrid[row][col] = Math.max(dpGrid[row + 1][col], dpGrid[row][col + 1]);
+                }
+            }
+        }
+        
+        // The original problem's answer is in dp_grid[0][0]. Return it.
+        return dpGrid[0][0];
+    }
 
 }
