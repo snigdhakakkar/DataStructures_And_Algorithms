f662e69922876c3cacbce1b7ed26699e40456a91
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-20 14:21:26 -0400

    find the longest common subsequence - dynamic programming with space optimization

--- LeetCode/src/Top75/longest_common_subsequence.java
+++ LeetCode/src/Top75/longest_common_subsequence.java
@@ -150,5 +150,38 @@ public class longest_common_subsequence {
         // The original problem's answer is in dp_grid[0][0]. Return it.
         return dpGrid[0][0];
     }
+    
+    public int longestCommonSubsequenceIII(String text1, String text2) {
+        //Approach 4 - dynamic programming with space optimization
+        
+        // If text1 doesn't reference the shortest string, swap them.
+        if (text2.length() < text1.length()){
+            String temp = text1;
+            text1 = text2;
+            text2 = temp;
+        }
+        
+        // The previous column starts with all 0's and like before is 1
+    // more than the length of the first word.
+        int[] previous = new int[text1.length() + 1];
+        
+        // Iterate through each column, starting from the last one.
+        for (int col = text2.length() - 1; col >= 0; col--){
+            // Create a new array to represent the current column.
+            int[] current = new int[text1.length() + 1];  
+             for (int row = text1.length() - 1; row >= 0; row--){
+                 if (text1.charAt(row) == text2.charAt(col)) {
+                     current[row] = 1 + previous[row + 1];
+                 } else {
+                     current[row] = Math.max(previous[row], current[row + 1]);
+                 }
+             }
+            // The current column becomes the previous one.
+            previous = current;
+        }
+        
+        // The original problem's answer is in previous[0]. Return it.
+        return previous[0];
+    }
 
 }
