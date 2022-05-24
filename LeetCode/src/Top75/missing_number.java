eb7a944802f61fb535eda813c6623eb973badfef
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-24 19:19:26 -0400

    updated

--- LeetCode/src/Top75/missing_number.java
+++ LeetCode/src/Top75/missing_number.java
@@ -26,7 +26,7 @@ public class missing_number {
         // Ensure that n is at the last index
         if (nums[nums.length-1] != nums.length) {
             return nums.length;
-        }
+        }   
         
         // Ensure that 0 is at the first index
         else if (nums[0] != 0) {
