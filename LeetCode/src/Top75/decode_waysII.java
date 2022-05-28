6843c0922e6386f99ce5dcf624c96f39bcf187fa
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-28 04:55:25 -0400

    no. of ways to decode dp approach

--- LeetCode/src/Top75/decode_waysII.java
+++ LeetCode/src/Top75/decode_waysII.java
@@ -73,5 +73,37 @@ public class decode_waysII {
 	        
 	        
 	    }
+	    
+	    //DP Approach
+	    //time compleity: O(N); space complexity: O(N)
+	    public int numDecodingsI(String s) {
+	        long[] dp = new long[s.length() + 1];
+	        dp[0] = 1; //base case
+	        dp[1] = s.charAt(0) == '*' ? 9 : s.charAt(0) == '0' ? 0 : 1;
+	         for (int i = 1; i < s.length(); i++) {
+	             if (s.charAt(i) == '*') {
+	                 dp[i + 1] = 9 * dp[i] % M;
+	                 if (s.charAt(i - 1) == '1'){
+	                     dp[i + 1] = (dp[i + 1] + 9 * dp[i - 1]) % M;
+	                 } else if (s.charAt(i - 1) == '2') {
+	                     dp[i + 1] = (dp[i + 1] + 6 * dp[i - 1]) % M;
+	                 } else if (s.charAt(i - 1) == '*') {
+	                      dp[i + 1] = (dp[i + 1] + 15 * dp[i - 1]) % M;
+	                 }
+	             } else {
+	                 dp[i + 1] = s.charAt(i) != '0' ? dp[i] : 0;
+	                 if (s.charAt(i - 1) == '1') {
+	                     dp[i + 1] = (dp[i + 1] + dp[i - 1]) % M;
+	                 } else if (s.charAt(i - 1) == '2' && s.charAt(i) <= '6') {
+	                     dp[i + 1] = (dp[i + 1] + dp[i - 1]) % M;
+	                 } else if (s.charAt(i - 1) == '*') {
+	                     dp[i + 1] = (dp[i + 1] + (s.charAt(i) <= '6' ? 2 : 1) * dp[i - 1]) % M;
+	                 }
+	             }
+	         }
+	        
+	         return (int) dp[s.length()];
+	        
+	    }
 
 }
