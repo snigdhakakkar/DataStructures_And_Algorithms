d63fcd7125a0fa69b0d53fa67f0ee84c3b0858bc
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-29 00:30:14 -0400

    rotating a matrix by 90 degrees

--- LeetCode/src/Top75/rotate_image.java
+++ LeetCode/src/Top75/rotate_image.java
@@ -9,6 +9,8 @@ input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
  */
 
 public class rotate_image {
+	
+	//Approach 1: Rotate Groups of Four Cells
 	//time complexity: O(M)  where M is the number of cells in the matrix
 	//space complexity: O(1)
 	public void rotate(int[][] matrix) {
@@ -23,5 +25,33 @@ public class rotate_image {
             }
         }
     }
+	
+	//Approach 2: Reverse on Diagonal and then Reverse Left to Right
+	public void rotateI(int[][] matrix) {
+        transpose(matrix);
+       reflect(matrix);
+   }
+   
+   public void transpose(int[][] matrix) {
+       int n = matrix.length;
+       for (int i = 0; i < n; i++){
+           for (int j = i+1; j < n; j++){
+               int tmp = matrix[j][i];
+               matrix[j][i] = matrix[i][j];
+               matrix[i][j] = tmp;
+           }
+       }
+   }
+   
+   public void reflect(int[][] matrix){
+       int n = matrix.length;
+       for (int i = 0; i < n; i++){
+           for (int j = 0; j < n / 2; j++) {
+               int tmp = matrix[i][j];
+               matrix[i][j] = matrix[i][n - j - 1];
+               matrix[i][n - j - 1] = tmp;
+           }
+       }
+   }
 
 }
