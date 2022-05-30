cde851a3f0f2a6700f3f7518c9587b11c97210a9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-05-30 17:30:37 -0400

    trienode class for word search

---
+++ LeetCode/src/Top75/trienode_.java
@@ -0,0 +1,13 @@
+package Top75;
+
+import java.util.HashMap;
+
+public class trienode_ {
+	
+	HashMap<Character, trienode_> children = new HashMap<Character, trienode_>();
+    String word = null;
+    public trienode_() {
+        
+    }
+
+}
