b937215d5a6aeac2a69ec7d28be50ca9cc97214a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-13 11:20:12 -0400

    Leetcode Blind75 array questions in Python

---
+++ Leetcode_Python/Blind75/Arrays/TwoSum.py
@@ -0,0 +1,11 @@
+from typing import List
+##Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+##You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+##bruteforce approach
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if(nums[j] == target - nums[i]):
+                    return [i,j]
