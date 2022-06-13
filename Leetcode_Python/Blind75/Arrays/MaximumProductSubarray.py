cdac96af5998d4684520767b5db32e7c20f3a6d5
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-13 16:23:09 -0400

    DP approach

--- Leetcode_Python/Blind75/Arrays/MaximumProductSubarray.py
+++ Leetcode_Python/Blind75/Arrays/MaximumProductSubarray.py
@@ -18,3 +18,22 @@ class Solution:
                 result = max(result, accumulator)
                 
         return result   
+
+## time complexity: O(N), Space complexity: O(1)
+    def maxProduct(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+        
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far*curr, min_so_far*curr)
+            min_so_far = min(curr, max_so_far*curr, min_so_far*curr)
+            
+            max_so_far = temp_max
+            
+            result = max(max_so_far, result)
+        return result
