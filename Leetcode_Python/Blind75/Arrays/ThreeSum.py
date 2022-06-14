25db465c0fe57b90d4adc369361fe35cd46dccba
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-14 12:15:59 -0400

    HashMap approach without sorting the actual list

--- Leetcode_Python/Blind75/Arrays/ThreeSum.py
+++ Leetcode_Python/Blind75/Arrays/ThreeSum.py
@@ -61,3 +61,16 @@ class Solution:
                     j +=1
             seen.add(nums[j])
             j += 1
+## using hashmap for seen and without sorting the actual List: time complexity-O(N^2), space complexity: O(N)
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res, dups = set(), set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 - val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
+        return res
