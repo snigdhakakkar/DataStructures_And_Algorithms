e7157b85efd606606b17c12c294c703a4de717f2
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-14 11:40:39 -0400

    One pass binary search approach

--- Leetcode_Python/Blind75/Arrays/SearchInARotatedSortedArray.py
+++ Leetcode_Python/Blind75/Arrays/SearchInARotatedSortedArray.py
@@ -66,3 +66,26 @@ class Solution:
         return search(0, rotateIndex)
             
 ##time complexity: O(logN), space complexity: O(1)
+
+##one-pass binary search approach
+##time complexity: O(logN), space complexity: O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[start]:
+                if target >= nums[start] and target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if target <= nums[end] and target > nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        
+        return -1
+
