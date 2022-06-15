8a3f0935f2a66b7f385ff24c2243c62d39d1d18a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-15 12:04:48 -0400

    Divide and conquer (masking, shifting and combining) approach

--- Leetcode_Python/Blind75/Binary/ReverseBits.py
+++ Leetcode_Python/Blind75/Binary/ReverseBits.py
@@ -12,7 +12,8 @@
 
 ##Approach 1: bit by bit reverse
 ## time complexity: O(1), space complexity: O(1)
-def reverseBits(self, n: int) -> int:
+class Solution:
+    def reverseBits(self, n: int) -> int:
         ret, power = 0, 31
         while n:
             ret += (n & 1) << power
@@ -20,4 +21,14 @@ def reverseBits(self, n: int) -> int:
             power -= 1
             
         return ret
+
+## Approach 2: divide and conquer approach
+## time complexity: O(1) and space complexity: O(1)
+    def reverseBits(self, n: int) -> int:
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+        return n
 
