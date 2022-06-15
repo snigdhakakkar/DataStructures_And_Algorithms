4dc9ecc7ceda80879fd30e2853027afb5d6ba651
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-15 14:48:20 -0400

    Memoization approach

--- Leetcode_Python/Blind75/DynamicProgramming/CoinChange.py
+++ Leetcode_Python/Blind75/DynamicProgramming/CoinChange.py
@@ -18,3 +18,17 @@ def coinChange(self, coins: List[int], amount: int) -> int:
                 dp[x] = min(dp[x], dp[x - coin] + 1)
         
         return dp[amount] if dp[amount] != float('inf') else -1 
+
+## Memoization approach
+def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = [[0 for j in range(amount+1)] for i in range(len(coins) + 1)]
+        for j in range(1, len(memo[0])):
+                memo[0][j] = float('inf')
+        for i in range(1, len(memo)):
+            for j in range(1, len(memo[0])):
+                if coins[i - 1] <= j:
+                    memo[i][j] = min(memo[i-1][j], memo[i][j-coins[i-1]]+1)
+                else:
+                    memo[i][j] = memo[i-1][j]
+        return memo[-1][-1] if memo[-1][-1] != float('inf') else -1
+                
