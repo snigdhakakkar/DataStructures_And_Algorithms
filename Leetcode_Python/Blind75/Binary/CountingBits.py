f3f89f30cf12337b3357aaae047af73b0d19808f
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-15 10:57:53 -0400

    MOST SIGNIFICANT BIT APPROACH

--- Leetcode_Python/Blind75/Binary/CountingBits.py
+++ Leetcode_Python/Blind75/Binary/CountingBits.py
@@ -21,4 +21,23 @@ class Solution:
             
         return ans
 
-    
+    ##Approach 2: DP + Most Significant Bit
+    ## In general, we have the following transition function for popcount P(x):P(x+b)=P(x)+1,b=2^m >x
+    ##time complexity: O(n) as we perform constant number of operations for each integer x, in the range 0 to n
+    ##space complexity: O(1)
+    def countBits(self, n: int) -> List[int]:
+        
+        ans = [0] * (n+1)
+        x = 0
+        b = 1
+        
+        # [0, b) is calculated
+        while b <= n:
+            # generate [b, 2b) or [b, n) from [0, b)
+            while x < b and x + b <= n:
+                ans[x+b] = ans[x] + 1
+                x += 1
+            x=0 # reset x
+            b <<=1 #b = 2b
+            
+        return ans
