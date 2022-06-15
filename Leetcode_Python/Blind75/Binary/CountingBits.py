bbf7831d6737d9d1544f7e5b6258512b6a1904ad
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-15 11:04:58 -0400

    LEAST SIGNIFICANT BIT APPROACH

--- Leetcode_Python/Blind75/Binary/CountingBits.py
+++ Leetcode_Python/Blind75/Binary/CountingBits.py
@@ -25,15 +25,12 @@ class Solution:
     ## In general, we have the following transition function for popcount P(x):P(x+b)=P(x)+1,b=2^m >x
     ##time complexity: O(n) as we perform constant number of operations for each integer x, in the range 0 to n
     ##space complexity: O(1)
-    def countBits(self, n: int) -> List[int]:
-        
+    def countBitsI(self, n: int) -> List[int]:
         ans = [0] * (n+1)
         x = 0
-        b = 1
-        
-        # [0, b) is calculated
-        while b <= n:
-            # generate [b, 2b) or [b, n) from [0, b)
+        b = 1 #[0, b) is calculated
+        while b <= n: 
+            #generate [b, 2b) or [b, n) from [0, b)
             while x < b and x + b <= n:
                 ans[x+b] = ans[x] + 1
                 x += 1
@@ -41,3 +38,20 @@ class Solution:
             b <<=1 #b = 2b
             
         return ans
+    
+    ## Approach 3: DP + Least Significant Bit
+    ## P(x) = P(x/2) + (x mod 2)
+    ##time complexity: O(n) as we perform constant number of operations for each integer x, in the range 0 to n
+    ##space complexity: O(1)
+    def countBitsII(self, n: int) -> List[int]:
+            
+        ans = [0] * (n+1)
+        for x in range(1, n+1):
+            #x/2 is x>>1 and x%2 is x&1
+            ans[x] = ans[x>>1] + (x&1)
+            
+        return ans
+
+
+
+    
