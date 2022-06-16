08757b82ba0593f57cbceaa44a2afbb9d0411ca8
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-16 13:48:36 -0400

    Longest Common subsequences - DP approach with space optimization

--- Leetcode_Python/Blind75/DynamicProgramming/LongestCommonSubsequence.py
+++ Leetcode_Python/Blind75/DynamicProgramming/LongestCommonSubsequence.py
@@ -77,3 +77,31 @@ class Solution:
         
         # The original problem's answer is in dp_grid[0][0]. Return it.
         return dp_grid[0][0]
+
+    ## Space optimization approach in DP: time complexity: O(MN), space complexity: O(min(M,N))
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        # if text1 doesn't reference the shortest string, swap them
+        if len(text2) < len(text1):
+            text1, text2 = text2, text1
+            
+        # The previous column starts with all 0's and like before is 1
+        # more than the length of the first word.
+        previous = [0]*(len(text1) + 1)
+        
+        #iterate up each column, starting from the last one
+        for col in reversed(range(len(text2))):
+            current = [0]*(len(text1) + 1)
+            for row in reversed(range(len(text1))):
+                if text2[col] == text1[row]:
+                    current[row] = 1 + previous[row + 1]
+                    
+                else:
+                    current[row] = max(previous[row], current[row + 1])
+                    
+            # The current column becomes the previous one.
+            previous = current
+        # The original problem's answer is in previous[0]. Return it.  
+        return previous[0]
+
+    
