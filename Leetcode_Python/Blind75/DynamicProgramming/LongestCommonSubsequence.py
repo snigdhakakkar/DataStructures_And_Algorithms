1d7b5c0c58cf60bd2ee90a96184a5ae9c6f91f4a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-16 11:39:31 -0400

    Longest Common subsequences - memoization approach

---
+++ Leetcode_Python/Blind75/DynamicProgramming/LongestCommonSubsequence.py
@@ -0,0 +1,35 @@
+## Given two strings text1 and text2, return the length of their longest common subsequence. 
+# If there is no common subsequence, return 0.
+# A subsequence of a string is a new string generated from the original string with 
+# some characters (can be none) deleted without changing the relative order of the remaining characters.
+# For example, "ace" is a subsequence of "abcde".
+# A common subsequence of two strings is a subsequence that is common to both strings.
+
+from functools import lru_cache
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        @lru_cache(maxsize=None)
+        def memo_solve(p1,p2):
+            
+            # Base case: If either string is now empty, we can't match
+            # up anymore characters.
+            if p1 == len(text1) or p2 == len(text2):
+                return 0
+            
+            # Option 1: We don't include text1[p1] in the solution.
+            option_1 = memo_solve(p1 + 1, p2)
+            
+            # Option 2: We include text1[p1] in the solution, as long as
+            # a match for it in text2 at or after p2 exists.
+            first_occurence = text2.find(text1[p1], p2)
+            option_2 = 0
+            if first_occurence != -1:
+                option_2 = 1 + memo_solve(p1 + 1, first_occurence + 1)
+                
+            # Return the best option.
+            return max(option_1, option_2)
+        
+        return memo_solve(0,0)
+        ## time complexity: O(M.N^2), space complexity: O(M.N)
