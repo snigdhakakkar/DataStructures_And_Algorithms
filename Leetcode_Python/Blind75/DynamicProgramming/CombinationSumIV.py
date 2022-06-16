2a45a4b6d80fc8a93ddb0a265d4fd57241aee360
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-16 15:03:25 -0400

    DP Bottom up approach

--- Leetcode_Python/Blind75/DynamicProgramming/CombinationSumIV.py
+++ Leetcode_Python/Blind75/DynamicProgramming/CombinationSumIV.py
@@ -20,3 +20,17 @@ def combinationSum4(self, nums: List[int], target: int) -> int:
             return result
         
         return combs(target)
+
+##DP Bottom-up approach: time complexity: O(T.N), space complexity: O(T)
+
+def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0 for i in range(target + 1)]
+        dp[0] = 1
+        
+        for comb_sum in range(target+1):
+            
+            for num in nums:
+                if comb_sum - num >= 0:
+                    dp[comb_sum] += dp[comb_sum - num]
+                    
+        return dp[target]
