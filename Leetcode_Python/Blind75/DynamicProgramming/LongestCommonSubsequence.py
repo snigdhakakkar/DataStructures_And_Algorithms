12b4e205ce515c092b77ba09e08ac1a29598a1fd
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-16 11:45:52 -0400

    Longest Common subsequences - Improved memoization approach

--- Leetcode_Python/Blind75/DynamicProgramming/LongestCommonSubsequence.py
+++ Leetcode_Python/Blind75/DynamicProgramming/LongestCommonSubsequence.py
@@ -8,6 +8,7 @@
 from functools import lru_cache
 
 class Solution:
+    ## MEMOIZATION APPROACH
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
         @lru_cache(maxsize=None)
@@ -33,3 +34,26 @@ class Solution:
         
         return memo_solve(0,0)
         ## time complexity: O(M.N^2), space complexity: O(M.N)
+        
+        ##IMPROVED MEMOIZATION
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        @lru_cache(maxsize=None)
+        def memo_solve(p1,p2):
+            
+            # Base case: If either string is now empty, we can't match
+            # up anymore characters.
+            if p1 == len(text1) or p2 == len(text2):
+                return 0
+            
+             # Recursive case 1.
+            if text1[p1] == text2[p2]:
+                return 1 + memo_solve(p1 + 1, p2 + 1)
+            
+            # Recursive case 2
+            else:
+                return max(memo_solve(p1, p2+1), memo_solve(p1+1, p2))
+        return memo_solve(0,0)
+
+        ## time complexity: O(M.N), space complexity: O(M.N)
