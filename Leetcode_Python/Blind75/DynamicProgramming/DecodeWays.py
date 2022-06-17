f72a6f6af4a554129c5920cc221e7e928ddc15b9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-17 11:43:46 -0400

    decode ways - Recursive approach with DP

--- Leetcode_Python/Blind75/DynamicProgramming/DecodeWays.py
+++ Leetcode_Python/Blind75/DynamicProgramming/DecodeWays.py
@@ -36,3 +36,29 @@ class Solution:
         
     def numDecodings(self, s: str) -> int:
         return self.recursiveWithMemo(0,s)
+
+## Approach 2: DP approach: time complexity - O(N), space complexity - O(N)
+
+    def numDecodings(self, s: str) -> int:
+        #DP array to store the subproblem results
+        dp = [0 for _ in range(len(s) + 1)]
+        
+        dp[0] = 1
+        #ways to decode a string of size 1 is 1; unless the string is '0'
+        #0 doesn't have a single digit decode
+        dp[1] = 0 if s[0] == '0' else 1
+        
+        for i in range(2, len(dp)):
+            
+            #check if successful single digit decode is possible
+            if s[i - 1] != '0':
+                dp[i] = dp[i - 1]
+                
+            #check if two digit decode is possible
+            two_digit = int(s[i - 2: i])
+            if two_digit >= 10 and two_digit <= 26:
+                dp[i] += dp[i - 2]
+                
+        return dp[len(s)]
+
+## Approach 3: DP with space optimization: time complexity - O(N), space complexity - O(1)
