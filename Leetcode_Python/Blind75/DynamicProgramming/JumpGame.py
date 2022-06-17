cdf32530ccd6a58981d19a5f8f495abd5e74a9b9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-17 14:58:35 -0400

    recursion approach

---
+++ Leetcode_Python/Blind75/DynamicProgramming/JumpGame.py
@@ -0,0 +1,24 @@
+## You are given an integer array nums. You are initially positioned at the array's first index, 
+# and each element in the array represents your maximum jump length at that position.
+# Return true if you can reach the last index, or false otherwise.
+
+## Approach 1: Recursion: time complexity - O(2^N), Space complexity - O(N)
+from typing import List
+
+
+def canJump(self, nums: List[int]) -> bool:
+        
+        
+        def canJumpFromPosition(position, nums) -> bool:
+            
+            if position == len(nums) - 1:
+                return True
+        
+            furthestJump = min((position + nums[position]), (len(nums) - 1))
+        
+            for nextPosition in range(position + 1, furthestJump + 1, 1):
+                if canJumpFromPosition(nextPosition, nums):
+                    return True
+        
+            return False
+        return canJumpFromPosition(0, nums)
