88430c74d76de5f216ec0654d5ba985a1467dc0a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-20 11:46:20 -0400

    postorder DFS approach

--- Leetcode_Python/Blind75/Graphs/courseSchedule.py
+++ Leetcode_Python/Blind75/Graphs/courseSchedule.py
@@ -51,3 +51,50 @@ def isCyclic(self, currCourse, courseDict, path):
         #after backtracking, remove the node from the path
         path[currCourse] = False
         return ret
+
+## Approach 2: Postorder DFS : time complexity - O(|E| + |V|) , space complexity: O(|E| + |V|)
+def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        from collections import defaultdict
+        courseDict = defaultdict(list)
+        
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]  
+            courseDict[prevCourse].append(nextCourse)
+        
+        checked = [False]*numCourses
+        path = [False]* numCourses
+        
+        for currCourse in range(numCourses):
+            if self.isCyclic(currCourse, courseDict, checked, path):
+                return False
+        return True
+    
+def isCyclic(self, currCourse, courseDict, checked, path):
+        ## bottom cases
+        if checked[currCourse]:
+            # this node has been checked, no cycle would be formed with this node.
+            return False
+        if path[currCourse]:
+            # come across a previously visited node, i.e. detect the cycle
+            return True
+        
+        #implement post order DFS on the children nodes
+        #mark the node in the path
+        path[currCourse] = True
+        
+        ret = False
+        ## postorder DFS to visit all the node's children first
+        for child in courseDict[currCourse]:
+            ret = self.isCyclic(child, courseDict, checked, path)
+            if ret:
+                break
+                
+        #after the visits of children, we come back to process the node itself
+        # remove the node from the path
+        path[currCourse] = False
+        
+        # Now that we've visited the nodes in the downstream,
+        #   we complete the check of this node.
+        checked[currCourse] = True
+        return ret
