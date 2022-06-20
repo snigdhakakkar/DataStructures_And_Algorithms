addafbfabdba5e578a1d9b20507edcd639fe56f5
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-20 06:45:05 -0400

    clone graph - breadth first search approach

--- Leetcode_Python/Blind75/Graphs/cloneGraph.py
+++ Leetcode_Python/Blind75/Graphs/cloneGraph.py
@@ -56,3 +56,57 @@ class Solution:
             clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
             
         return clone_node
+
+##Apprach 2: breadth-first search
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, neighbors):
+        self.val = val
+        self.neighbors = neighbors
+
+from collections import deque
+
+class Solution(object):
+
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+
+        if not node:
+            return node
+
+        # Dictionary to save the visited node and it's respective clone
+        # as key and value respectively. This helps to avoid cycles.
+        visited = {}
+
+        # Put the first node in the queue
+        queue = deque([node])
+        # Clone the node and put it in the visited dictionary.
+        visited[node] = Node(node.val, [])
+
+        # Start BFS traversal
+        while queue:
+            # Pop a node say "n" from the from the front of the queue.
+            n = queue.popleft()
+            # Iterate through all the neighbors of the node
+            for neighbor in n.neighbors:
+                if neighbor not in visited:
+                    # Clone the neighbor and put in the visited, if not present already
+                    visited[neighbor] = Node(neighbor.val, [])
+                    # Add the newly encountered node to the queue.
+                    queue.append(neighbor)
+                # Add the clone of the neighbor to the neighbors of the clone node "n".
+                visited[n].neighbors.append(visited[neighbor])
+
+        # Return the clone of the node from visited.
+        return visited[node]
+
+## Time Complexity : O(N + M), where N is a number of nodes (vertices) and M is a number of edges.
+
+## Space Complexity : O(N). This space is occupied by the visited dictionary and in addition to that, 
+# space would also be occupied by the queue since we are adopting the BFS approach here. 
+# The space occupied by the queue would be equal to O(W) where WW is the width of the graph. 
+# Overall, the space complexity would be O(N).
