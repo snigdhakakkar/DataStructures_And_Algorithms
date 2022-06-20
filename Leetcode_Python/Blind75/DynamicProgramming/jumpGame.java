ee84f6603969bd53d76da24d582fd8f992c7759a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-20 05:35:40 -0400

    DP bottom up appraoch

--- Leetcode_Python/Blind75/DynamicProgramming/jumpGame.java
+++ Leetcode_Python/Blind75/DynamicProgramming/jumpGame.java
@@ -43,3 +43,29 @@ class Solution {
  * Space complexity: O(2n) = O(n) . First n originates from recursion. 
  * Second n comes from the usage of the memo table.
  */
+
+//Approach 2: DP bottom up approach
+class SolutionI {
+    
+    public boolean canJump(int[] nums){
+        Index[] memo = new Index[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            memo[i] = Index.UNKNOWN;
+        }
+        
+        memo[nums.length - 1] = Index.GOOD;
+        
+        for(int i = nums.length - 2; i >=0; i--){
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++){
+                if(memo[j] == Index.GOOD){
+                    memo[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+        
+        return memo[0] == Index.GOOD;
+    }
+     
+ }
