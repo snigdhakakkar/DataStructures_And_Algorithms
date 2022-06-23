93c5092ab0edaeb31d93d5badf439c3c255c840e
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-23 11:59:56 -0400

    Check whether a graph is a valid tree using iterative DFS

--- Leetcode_Python/Blind75/Graphs/GraphValidTree.py
+++ Leetcode_Python/Blind75/Graphs/GraphValidTree.py
@@ -1,4 +1,4 @@
-##Iterative DFS approach - time complexity: O(N + M), space complexity - O(N + M)
+##Recursive DFS approach - time complexity: O(N + M), space complexity - O(N + M)
 from typing import List
 
 
@@ -27,4 +27,27 @@ def validTree(self, n: int, edges: List[List[int]]) -> bool:
         
         return dfs(0, -1) and n == len(visit) ##checking if all the nodes are visited and the result of dfs is true i.e. there is no loop
             
-        
+##Iterative DFS
+def validTree(self, n: int, edges: List[List[int]]) -> bool:
+    
+    if len(edges) != n - 1: return False
+    
+    adj_list = [[] for _ in range(n)]
+    for A, B in edges:
+        adj_list[A].append(B)
+        adj_list[B].append(A)
+    
+    parent = {0: -1}
+    stack = [0]
+    
+    while stack:
+        node = stack.pop()
+        for neighbour in adj_list[node]:
+            if neighbour == parent[node]:
+                continue
+            if neighbour in parent:
+                return False
+            parent[neighbour] = node
+            stack.append(neighbour)
+    
+    return len(parent) == n  
