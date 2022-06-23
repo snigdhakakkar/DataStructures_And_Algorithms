1dbdd74aeabc1ad0ddb8eaae5d467de376bc7d20
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-23 16:40:25 -0400

    approach 2: First occurence transformation

--- Leetcode_Python/PracticeQuestions/Strings/IsomorphicStrings.py
+++ Leetcode_Python/PracticeQuestions/Strings/IsomorphicStrings.py
@@ -25,3 +25,21 @@ def isIsomorphic(self, s: str, t: str) -> bool:
                 return False
             
         return True
+
+### Approach 2: First occurence transformation
+## time complexity - O(N), space complexity - O(N)
+
+
+def isIsomorphic(self, s: str, t: str) -> bool:
+        return self.transformString(s) == self.transformString(t)
+    
+def transformString(self, s: str) -> str:
+        index_mapping = {}
+        new_str = []
+        
+        for i, c in enumerate(s):
+            if c not in index_mapping:
+                index_mapping[c] = i
+            new_str.append(str(index_mapping[c]))
+            
+        return " ".join(new_str)
