3141d62fcb43aaca32052e417cf195728c957723
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-24 13:59:12 -0400

    merge two lists using iterative approach

--- Leetcode_Python/PracticeQuestions/LinkedList/MergeTwoSortedLists.py
+++ Leetcode_Python/PracticeQuestions/LinkedList/MergeTwoSortedLists.py
@@ -3,10 +3,10 @@
 # Return the head of the merged linked list.
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+        def __init__(self, val=0, next=None):
+          self.val = val
+          self.next = next
 ##Approach 1: Recursion - time complexity: O(M + N), space complexity: O(M + N)
 class Solution:
     def mergeTwoLists(self, l1, l2):
@@ -20,3 +20,22 @@ class Solution:
         else:
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
+    ##ITERATIVE APPROACH: time complexity - O(M+N), Space complexity - O(1)
+    def mergeTwoListsI(self, l1, l2):
+        ##maintain an unchanging reference to node ahead of the return node
+        head = ListNode(-1)
+        
+        tail = head
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+            
+        #atleast one of l1 and l2 can still have remaining nodes at this point, so connecting the non-null list to the end of the merged list
+        tail.next = l1 if l1 is not None else l2
+        
+        return head.next
