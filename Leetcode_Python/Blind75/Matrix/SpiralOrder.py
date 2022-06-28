fe6290a768324423e3ca897a8d5757fb8b9cb473
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-28 15:44:59 -0400

    spiral order - marking as visited approach

--- Leetcode_Python/Blind75/Matrix/SpiralOrder.py
+++ Leetcode_Python/Blind75/Matrix/SpiralOrder.py
@@ -1,5 +1,5 @@
 ## Given an m x n matrix, return all elements of the matrix in spiral order.
-#time complexity - O(MN), space complexity - O(1)
+#Approach 1 - Moving the boundaries: time complexity - O(MN), space complexity - O(1)
 
 from typing import List
 
@@ -40,3 +40,51 @@ class Solution:
             down -= 1
             
         return result
+
+## Approach 2: Marking the seen elements as VISITED : time complexity - O(MN), space complexity - O(1)
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        VISITED = 101
+        rows, columns = len(matrix), len(matrix[0])
+        # Four directions that we will move: right, down, left, up.
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        # Initial direction: moving right.
+        current_direction = 0
+        # The number of times we change the direction.
+        change_direction = 0
+         # Current place that we are at is (row, col).
+        # row is the row index; col is the column index.
+        row = col = 0
+        # Store the first element and mark it as visited.
+        result = [matrix[0][0]]
+        matrix[0][0] = VISITED
+        
+        while change_direction < 2:
+            
+            while True:
+                # Calculate the next place that we will move to.
+                next_row = row + directions[current_direction][0]
+                next_col = col + directions[current_direction][1]
+                
+                #break if the next step is out of bounds
+                if not (0 <= next_row < rows and 0 <= next_col < columns):
+                    break
+                # Break if the next step is on a visited cell.
+                if matrix[next_row][next_col] == VISITED:
+                    break
+                    
+                # Reset this to 0 since we did not break and change the direction.
+                change_direction = 0
+                # Update our current position to the next step.
+                row, col = next_row, next_col
+                result.append(matrix[row][col])
+                matrix[row][col] = VISITED
+                
+           # Change our direction
+            current_direction = (current_direction + 1) % 4
+            # Increment change_direction because we changed our direction.
+            change_direction += 1
+            
+        return result
+                
+            
