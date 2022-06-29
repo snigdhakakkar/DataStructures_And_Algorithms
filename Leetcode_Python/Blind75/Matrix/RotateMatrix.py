1aaccbd5993d857343fa0ad4598bf85085d6e9a0
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-29 10:04:28 -0400

    rotating a group of cells approach

---
+++ Leetcode_Python/Blind75/Matrix/RotateMatrix.py
@@ -0,0 +1,21 @@
+##You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
+# DO NOT allocate another 2D matrix and do the rotation.
+
+##  Approach 1: Rotate groups of 4 cells - time complexity: O(M), space complexity: O(1)
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n//2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
