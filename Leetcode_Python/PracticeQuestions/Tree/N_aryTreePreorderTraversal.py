013e5b9cdbc77fd9c311054d3b44a5245413c9bb
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-29 16:27:02 -0400

    preorder traversal iterative approach

--- Leetcode_Python/PracticeQuestions/Tree/N_aryTreePreorderTraversal.py
+++ Leetcode_Python/PracticeQuestions/Tree/N_aryTreePreorderTraversal.py
@@ -5,6 +5,7 @@
 ##iterative approach - time complexity - O(N), space complexity - O(N)
 
 # Definition for a Node.
+from collections import deque
 from typing import List
 
 
@@ -27,3 +28,39 @@ class Solution:
             stack.extend(root.children[::-1])
             
         return output
+
+##Approach 2: Recursive solution
+class Solution:
+    
+    def preorder(self, root: 'Node') -> List[int]:
+        result = []
+        if root is None:
+            return []
+    
+        def preorderhelper(node):
+            if not node:
+                return
+            result.append(node.val)
+            for child in node.children:
+                preorderhelper(child)
+            
+        preorderhelper(root)
+        return result
+
+##Approach 3: Iterative using a queue
+class Solution:
+    
+    def preorder(self, root: 'Node') -> List[int]:
+        result = []
+        if root is None:
+            return []
+    
+        q = deque([root])
+        
+        while q:
+            cand = q.popleft()
+            result.append(cand.val)
+            for c in reversed(cand.children):
+                q.appendleft(c)
+                
+        return result
