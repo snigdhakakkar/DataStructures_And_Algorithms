735243593635572c4f066fa52840668075fca6de
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-29 10:36:32 -0400

    transpose and reflect approach

--- Leetcode_Python/Blind75/Matrix/RotateMatrix.py
+++ Leetcode_Python/Blind75/Matrix/RotateMatrix.py
@@ -49,3 +49,25 @@ class SolutionI:
                 
             r -= 1
             l += 1
+
+##Approach 3: transpose and reverse - Time complexity: O(M), space complexity - O(1)
+
+class SolutionII:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self.transpose(matrix)
+        self.reflect(matrix)
+        
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+                
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
