7d6fc5d8a8ca2045bbecf9960a8a13370ad31e64
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-29 14:31:07 -0400

    sliding window approach

--- Leetcode_Python/Blind75/String/LongestSubstringWithoutRepeatingCharacters.py
+++ Leetcode_Python/Blind75/String/LongestSubstringWithoutRepeatingCharacters.py
@@ -61,3 +61,22 @@ class Solution:
             res = max(res, r - l + 1)
 
         return res
+
+##modified sliding window using a dictionary in place of a set
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        # mp stores the current index of a character
+        mp = {}
+        
+        i = 0
+        # try to extend the range [i, j]
+        for j in range(n):
+            if s[j] in mp:
+                i = max(mp[s[j]], i)
+            
+            ans = max(ans, j - i + 1)
+            mp[s[j]] = j + 1
+            
+        return ans
