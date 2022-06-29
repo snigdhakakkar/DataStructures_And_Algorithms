d66eec5aae07f6b1a3b5bc1dfc93af7ab1c48b8e
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-29 10:24:04 -0400

    rotating a group of cells approach

--- Leetcode_Python/Blind75/Matrix/RotateMatrix.py
+++ Leetcode_Python/Blind75/Matrix/RotateMatrix.py
@@ -19,3 +19,33 @@ class Solution:
                 matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
                 matrix[j][n - 1 - i] = matrix[i][j]
                 matrix[i][j] = tmp
+
+##Approach 2: Using pointers for rotation - time complexity: O(M^2), space complexity: O(1)
+
+class SolutionI:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r = 0, len(matrix) - 1
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+                
+                #save the top left value in a temp variable
+                topLeft = matrix[top][l + i]
+                
+                #move the bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+                
+                #move the bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+                
+                #move the top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+                
+                #move the top left into the top right
+                matrix[top + i][r] = topLeft
+                
+            r -= 1
+            l += 1
