a807ab7fee4a45a19518d234ed0d306cba6bc57c
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-29 11:13:35 -0400

    backtracking - without cleaning up

--- Leetcode_Python/Blind75/Matrix/WordSearch.py
+++ Leetcode_Python/Blind75/Matrix/WordSearch.py
@@ -36,4 +36,40 @@ class Solution:
                     return True
                 
         return False
+
+##Approach 2: backtracking - time complexity: O(N.3^L), space complexity: O(L) where L is the length of the string
+class Solution(object):
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.ROWS, self.COLS = len(board), len(board[0])
+        self.board = board
+        
+        for r in range(self.ROWS):
+            for c in range(self.COLS):
+                if self.dfs(r,c, word):
+                    return True
+                
+        return False
     
+    def dfs(self, r, c, suffix):
+        if len(suffix) == 0:
+            return True
+            
+        if (r < 0 or c < 0 or r >= self.ROWS or c >= self.COLS 
+            or self.board[r][c] != suffix[0]):
+            return False
+        
+        result = False
+        # mark the choice before exploring further.
+        self.board[r][c] = '#'
+        
+        # explore the 4 neighbor directions
+        for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            result = self.dfs(r + rowOffset, c + colOffset, suffix[1:])
+            # break instead of return directly to do some cleanup afterwards
+            if result:
+                return True
+                
+        #revert the change, a clean slate and no side-effect
+        self.board[r][c] = suffix[0]
+        
+        return result
