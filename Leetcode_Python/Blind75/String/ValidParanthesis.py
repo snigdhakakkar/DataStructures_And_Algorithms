2b6999b2263413f1133570ae4b6aa9cda9fa1ff8
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-30 13:25:10 -0400

    using stack and hashmap

---
+++ Leetcode_Python/Blind75/String/ValidParanthesis.py
@@ -0,0 +1,21 @@
+##Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+# determine if the input string is valid.
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+##Approach: using stack and hashmap : time complexity: O(N), space complexity: O(N)
+def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen =  {")" : "(", "]":"[", "}":"{"}
+        
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                    
+                else:
+                    return False
+                
+            else:
+                stack.append(c)
+        return True if not stack else False
