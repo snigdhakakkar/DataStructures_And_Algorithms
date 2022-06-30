ff0a8c20af534f7d1344707badf67548fe7839d9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-06-30 13:45:14 -0400

    using left and right pointers

---
+++ Leetcode_Python/PracticeQuestions/BinarySearch/binarySearch.py
@@ -0,0 +1,22 @@
+##Given an array of integers nums which is sorted in ascending order, and an integer target, 
+# write a function to search target in nums. If target exists, then return its index. 
+# Otherwise, return -1. You must write an algorithm with O(log n) runtime complexity.
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left, right= 0, len(nums) - 1
+        
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if nums[pivot] == target:
+                return pivot
+            if target < nums[pivot]:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+                
+        return -1
