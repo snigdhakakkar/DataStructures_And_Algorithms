1aaeb717da25009cddee4cb7e5bd69aab93eabf9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-04 14:40:14 -0400

    iterative traversal with valid range

--- Leetcode_Python/PracticeQuestions/Tree/validBinarySearchTree.py
+++ Leetcode_Python/PracticeQuestions/Tree/validBinarySearchTree.py
@@ -31,3 +31,24 @@ class Solution:
                    validate(node.left, low, node.val))
         
         return validate(root)
+
+##Approach 2: Iterative traversal with a valid range- time complexity: O(N), space complexity: O(N)
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        
+        if not root:
+            return True
+        
+        stack = [(root, -math.inf, math.inf)]
+        while stack:
+            root, lower, upper = stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            stack.append((root.right, val, upper))
+            stack.append((root.left, lower, val))
+            
+        return True
