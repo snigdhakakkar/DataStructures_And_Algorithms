063f97521f250054a0e1062317e2663d0f2f5fc9
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-04 10:42:49 -0400

    encoded and decoded using substring length and delimiter #

---
+++ Leetcode_Python/Blind75/String/EncodeAndDecodeStrings.py
@@ -0,0 +1,39 @@
+##Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back 
+# to the original list of strings.
+
+##time complexity: O(n) where n is the total number of characters in the string
+from typing import List
+
+
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        result = ""
+        for s in strs:
+            result += str(len(s)) + "#" + s
+            
+        return result
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        result, i = [], 0
+        
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            result.append(s[j + 1: j + 1 + length])
+            i = j + 1 + length
+            
+        return result
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
+
