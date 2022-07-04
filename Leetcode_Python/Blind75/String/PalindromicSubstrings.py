a732039f43e4e7e74216e2478ac0e7e34a06b0c0
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-04 10:11:45 -0400

    starting from each ith element and expanding outwards

---
+++ Leetcode_Python/Blind75/String/PalindromicSubstrings.py
@@ -0,0 +1,23 @@
+#Given a string s, return the number of palindromic substrings in it.
+# A string is a palindrome when it reads the same backward as forward.
+# A substring is a contiguous sequence of characters within the string.
+##time complexity: O(n^2); space complexity: O(1)
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        result = 0 ##count of palindromic substrings
+        
+        for i in range(len(s)):
+            l = r = i ##taking each element at eahc ith index for odd-length palindromes
+            while l >= 0 and r < len(s) and s[l] == s[r]: ##checking if the string is palindromic and within bounds
+                result += 1 ##increment the palindrome counter
+                l -= 1 ##expanding on both sides
+                r += 1
+                
+            l = i
+            r = i + 1 ##modifying the code for even-length palindromes
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                result += 1
+                l -= 1
+                r += 1
+                
+        return result
