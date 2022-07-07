48cad7eb05021a07c566fbe8510aa1df4f44073d
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-07 09:54:52 -0400

    recursive approach

--- Leetcode_Python/Blind75/Tree/MaximumDepth.py
+++ Leetcode_Python/Blind75/Tree/MaximumDepth.py
@@ -1,2 +1,18 @@
 ##Given the root of a binary tree, return its maximum depth.
 # A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+##Approach 1: Recrusion - time complexity: O(N), space complexity in worst case: O(N) & in best case: O(logN)
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height, right_height) + 1
