3fdd649d221a6d2b363329cf1c4f56c7ec3040e5
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-07 10:51:41 -0400

    iterative approach

--- Leetcode_Python/Blind75/Tree/InvertABinaryTree.py
+++ Leetcode_Python/Blind75/Tree/InvertABinaryTree.py
@@ -2,6 +2,9 @@
 
 ##Approach 1: recursive : time complexity - O(N), space complexity - O(logN) for a balanced tree; O(N) for an imbalanced tree
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -19,3 +22,23 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+##Iterative Approach
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        
+        q = deque([root])
+        while q:
+            curr = q.popleft()
+            curr.left, curr.right = curr.right, curr.left
+            
+            if curr.left:
+                q.append(curr.left)
+                
+            if curr.right:
+                q.append(curr.right)
+                
+        return root
+        
