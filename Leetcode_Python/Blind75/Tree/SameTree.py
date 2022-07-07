a0d3d24a9203481a6b0336f6f10b29f7a51dd5f4
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-07 10:38:48 -0400

    iterative approach

--- Leetcode_Python/Blind75/Tree/SameTree.py
+++ Leetcode_Python/Blind75/Tree/SameTree.py
@@ -3,6 +3,9 @@
 
 ##Approach 1: Recursion - time complexity: O(N), space complexity: O(N) for unbalanced tree; O(logN) for a balanced tree
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -17,3 +20,29 @@ class Solution:
         if p.val != q.val:
             return False
         return self.isSameTree(p.right,q.right) and self.isSameTree(p.left, q.left)
+
+## Approach 2: Iteration: time complexity: O(N), space complexity: O(N) for unbalanced tree; O(logN) for a balanced tree
+
+class Solution: 
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        
+        def check(p,q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+        
+        deq = deque([(p,q),])
+        while deq:
+            p,q = deq.popleft()
+            if not check(p,q):
+                return False
+            
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+                
+        return True
