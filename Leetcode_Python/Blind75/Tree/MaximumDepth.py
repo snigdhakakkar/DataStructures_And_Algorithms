5b54139f437d7f60472f77cfe15b862f9cc2a497
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-07 10:13:29 -0400

    iterative with DFS approach

--- Leetcode_Python/Blind75/Tree/MaximumDepth.py
+++ Leetcode_Python/Blind75/Tree/MaximumDepth.py
@@ -41,3 +41,22 @@ class Solution:
                 
             level += 1
         return level
+
+## Approach 3: Iterative with DFS:  time complexity: O(N), space complexity: O(N) or O(logN)
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        stack = [[root, 1]]
+        res = 1
+        
+        while stack:
+            node, depth = stack.pop()
+            
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+                
+        return res
