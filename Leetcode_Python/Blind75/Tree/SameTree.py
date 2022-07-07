126181a5983cd4533382f8586152fe86c7078a5e
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-07 10:29:03 -0400

    recursive approach

---
+++ Leetcode_Python/Blind75/Tree/SameTree.py
@@ -0,0 +1,19 @@
+## Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+##Approach 1: Recursion - time complexity: O(N), space complexity: O(N) for unbalanced tree; O(logN) for a balanced tree
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right,q.right) and self.isSameTree(p.left, q.left)
