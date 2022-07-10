7e6947782e399c542b40975627fe41ad3285e166
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-10 10:47:05 -0400

    updated

--- Leetcode_Python/Blind75/Tree/ConstructABinaryTreeFromPreorderAndInorderTraversal.py
+++ Leetcode_Python/Blind75/Tree/ConstructABinaryTreeFromPreorderAndInorderTraversal.py
@@ -4,10 +4,10 @@
 ##Recursive Approach
 from typing import List
 
-
+   
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
-        self.val = val
+        self.val = val 
         self.left = left
         self.right = right
 class Solution:
