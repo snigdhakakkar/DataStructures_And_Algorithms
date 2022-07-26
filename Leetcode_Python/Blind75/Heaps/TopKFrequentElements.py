9311a9406ec4dbc3c74b735288b9b55d6872efe7
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-26 15:40:45 -0400

    heap solution

--- Leetcode_Python/Blind75/Heaps/TopKFrequentElements.py
+++ Leetcode_Python/Blind75/Heaps/TopKFrequentElements.py
@@ -3,6 +3,8 @@
 
 ##bucket sort approach: time complexity: O(n), space complexity: O(n)
 
+import heapq
+from collections import Counter
 from typing import List
 
 
@@ -26,6 +28,22 @@ class Solution:
                 if len(res) == k:
                     return res
 
+##Approach 2:building a heap and converting it into output array
+class SolutionI:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        # O(1) time 
+        if k == len(nums):
+            return nums
+
+        # 1. build hash map : character and how often it appears
+        # O(N) time
+        count = Counter(nums)  
+        # 2-3. build heap of top k frequent elements and
+        # convert it into an output array
+        # O(N log k) time
+        return heapq.nlargest(k, count.keys(), key = count.get)
+
+        
 
 
 
