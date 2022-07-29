2dac4e8912a1058f9e9fa7cd670daa1d882d1c09
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-29 14:45:52 -0400

    starting from previous pointer and not from the beginning

---
+++ Leetcode_Python/Facebook_Interview/ReadNCharactersGivenRead4_CallMultipleTimes.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        self.buf4 = [''] * 4
+        self.curr_used = 0
+        self.curr_read = 0
+        self.EOF = False
+        
+    def read(self, buf: List[str], n: int) -> int:
+        
+        num_read = 0
+        while num_read < n and not self.EOF:
+            if self.curr_used == self.curr_read:
+                self.curr_read = read4(self.buf4)
+                self.curr_used = 0
+                if self.curr_read == 0:
+                    self.EOF = True
+            else:
+                delta = min(self.curr_read - self.curr_used, n - num_read)
+                buf[num_read: num_read + delta] = self.buf4[self.curr_used:
+                                                       self.curr_used + delta]
+                num_read += delta
+                self.curr_used += delta
+
+        return num_read
