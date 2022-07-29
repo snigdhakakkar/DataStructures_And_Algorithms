e69fe4fddb16d5f2c48c6e0b614efd18c829a4d5
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-07-29 13:13:23 -0400

    two pointer approach

---
+++ Leetcode_Python/Facebook_Interview/ValidPalindrome.py
@@ -0,0 +1,22 @@
+## A phrase is a palindrome if, after converting all uppercase letters into lowercase 
+# letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+# Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+                
+            if s[i].lower() != s[j].lower():
+                return False
+            
+            i += 1
+            j -= 1
+            
+        return True
