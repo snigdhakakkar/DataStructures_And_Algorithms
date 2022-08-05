1aa0497acbf72cbb9db3a8628bf3d73fdfb888b2
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-08-05 06:13:51 -0400

    using counter to check and enumerate to return index

---
+++ Leetcode_Python/Amazon_Interview/FirstUniqueCharacterInAString.py
@@ -0,0 +1,16 @@
+## Given a string s, find the first non-repeating character in it and return its index. 
+# If it does not exist, return -1.
+
+import collections
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        ## build hash map : character and how often it appears
+        count = collections.Counter(s)
+        
+        # find the index
+        for idx, char in enumerate(s):
+            if count[char] == 1:
+                return idx
+        return -1
