8075eb6264c860cd31db38293847f816945855de
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-08-05 06:23:05 -0400

    using a stack

---
+++ Leetcode_Python/Amazon_Interview/ValidParanthesis.py
@@ -0,0 +1,22 @@
+## Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+# determine if the input string is valid.
+# 
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {")" : "(", "]" : "[", "}" : "{"}
+        
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+                
+        return True if not stack else False
