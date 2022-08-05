77efc1f1e8915fe6823dd76e5128d640749e0375
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-08-05 06:01:35 -0400

    sum of 0-n numbers minus the sum of nums  approach

--- Leetcode_Python/Amazon_Interview/MissingNumber.py
+++ Leetcode_Python/Amazon_Interview/MissingNumber.py
@@ -12,3 +12,14 @@ class Solution:
             missing ^= i ^ num
         return missing
 
+#Approach 2: Sum(from 0 to n) - sum(nums)
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+        
+        for i in range(len(nums)):
+            res += (i-nums[i])
+            
+        return res
+
