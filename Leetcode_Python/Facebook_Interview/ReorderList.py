ceed5d0bf8a906ac8e9b70915fbeb697c0896e39
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-08-09 04:20:40 -0400

    find the middle of the list, reverse the second half and merge

---
+++ Leetcode_Python/Facebook_Interview/ReorderList.py
@@ -0,0 +1,50 @@
+##You are given the head of a singly linked-list. The list can be represented as:
+# L0 → L1 → … → Ln - 1 → Ln
+# Reorder the list to be on the following form:
+# 
+# L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+# You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+# Input: head = [1,2,3,4,5]
+# Output: [1,5,2,4,3]
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        #find middle
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #reverse the second half
+        second = slow.next
+        slow.next = None
+        prev = None
+        
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+            
+        #merge two halves of the list
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
+            
+            
+
+
