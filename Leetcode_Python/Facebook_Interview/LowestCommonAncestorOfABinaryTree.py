3a1479296ead807d0bfb18aeffe1cdf24a989ee8
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-08-11 07:26:10 -0400

    dfs approach

---
+++ Leetcode_Python/Facebook_Interview/LowestCommonAncestorOfABinaryTree.py
@@ -0,0 +1,39 @@
+## Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between 
+# two nodes p and q as the lowest node in T that has both p and q as descendants 
+# (where we allow a node to be a descendant of itself).”
+
+# Example -
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+# Output: 3
+# Explanation: The LCA of nodes 5 and 1 is 3.
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(curr):
+            if not curr:
+                return None
+            
+            if curr == p or curr == q:
+                return curr
+            
+            left = dfs(curr.left)
+            right = dfs(curr.right)
+            
+            #first case
+            if left and right:
+                return curr
+            
+            #second case
+            return left if left else right
+        
+        return dfs(root)
+            
+
