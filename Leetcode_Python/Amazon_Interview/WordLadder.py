ab514264dfaeb432a8ccda543291a74ce333da20
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-08-12 06:41:48 -0400

    used a queue and pattern matching dictionary

---
+++ Leetcode_Python/Amazon_Interview/WordLadder.py
@@ -0,0 +1,63 @@
+## A transformation sequence from word beginWord to word endWord using a dictionary wordList 
+# is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+#
+# Every adjacent pair of words differs by a single letter.
+# Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+# sk == endWord
+# Given two words, beginWord and endWord, and a dictionary wordList, 
+# return the number of words in the shortest transformation sequence from beginWord to endWord, 
+# or 0 if no such sequence exists.
+# 
+# Example 1:
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+# Output: 5
+# Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", 
+# which is 5 words long.
+# 
+# Approach 1: BFS - time complexity: O(N^2.M) where N is the length of words in wordList 
+# and M is the length of word
+
+from collections import deque
+import collections
+from typing import List
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        
+        nei = collections.defaultdict(list)
+        wordList.append(beginWord)
+        
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j+1:]
+                nei[pattern].append(word)
+                
+        visit = set([beginWord])
+        
+        q = deque([beginWord])
+        
+        res = 1
+        
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+            res += 1
+        return 0
+        
+        
+        
+        
+        
