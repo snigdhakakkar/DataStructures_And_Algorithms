991df073172f44e98be98cb3e5eef57142fabe6a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-08-18 20:00:00 -0600

    updated

--- Leetcode_Python/Amazon_Interview/ValidParanthesis.py
+++ Leetcode_Python/Amazon_Interview/ValidParanthesis.py
@@ -9,7 +9,7 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         closeToOpen = {")" : "(", "]" : "[", "}" : "{"}
-           
+              
         for c in s:
             if c in closeToOpen:
                 if stack and stack[-1] == closeToOpen[c]:
