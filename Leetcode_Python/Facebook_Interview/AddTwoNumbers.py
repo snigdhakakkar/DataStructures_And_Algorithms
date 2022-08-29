4afa016cf22385193b71927629e7f8cee58e8319
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-08-28 20:00:00 -0600

    updated

--- Leetcode_Python/Facebook_Interview/AddTwoNumbers.py
+++ Leetcode_Python/Facebook_Interview/AddTwoNumbers.py
@@ -35,7 +35,7 @@ class Solution:
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
             
-            
+               
             
         return dummy.next
             
