5ad988bcf429873e4de33020345fc7db42e6156a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-09-13 06:35:54 -0400

    two pointer approach

---
+++ Leetcode_Python/Neetcode150/ValidPalindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l,r = 0, len(s) - 1
+        
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+                
+            while l < r and not self.alphaNum(s[r]):
+                r -= 1
+                
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l,r = l + 1, r - 1
+            
+        return True
+            
+        
+        
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+        ord('a') <= ord(c) <= ord('z') or
+        ord('0') <= ord(c) <= ord('9'))
