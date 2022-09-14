8256e802c040baccf45012d24afb4810103767d8
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-09-14 05:41:26 -0400

    Use two pointer. Increment left pointer if height[l] < height[r]. MaxArea is max(maxArea, min(h[l],h[r])*width)

---
+++ Leetcode_Python/Neetcode150/ContainerWithMostWater.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxarea = 0
+        left = 0
+        right = len(height) - 1
+        
+        while left < right:
+            width = right - left
+            maxarea = max(maxarea, min(height[left], height[right])*width)
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+                
+        return maxarea
