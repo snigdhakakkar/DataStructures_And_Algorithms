bd4a034a22753766b04e018722d73c6c941d6cbf
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-09-16 16:56:40 -0400

    stack usage and 3 conditions with an inside function backTrack

---
+++ Leetcode_Python/Neetcode150/GenerateParantheses.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        #add open parantheses only if open count is less than n
+        #add closing parantheses only if close count is less than open
+        #valid iff open == close == n
+        
+        stack = []
+        res = []
+        
+        def backTrack(openN, closeN):
+            if openN == closeN == n:
+                res.append("".join(stack))
+                return
+            
+            if openN < n:
+                stack.append("(")
+                backTrack(openN + 1, closeN)
+                stack.pop()
+                
+            if closeN < openN:
+                stack.append(")")
+                backTrack(openN, closeN + 1)
+                stack.pop()
