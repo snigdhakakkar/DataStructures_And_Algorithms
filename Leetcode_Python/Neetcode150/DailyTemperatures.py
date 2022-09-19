f1dfb36e65b13335eded52f9e4a8c7d4002062d3
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-09-19 05:35:23 -0400

    monotonic stack problem

--- Leetcode_Python/Neetcode150/DailyTemperatures.py
+++ Leetcode_Python/Neetcode150/DailyTemperatures.py
@@ -16,3 +16,19 @@ class Solution:
             stack.append([t,i])
             
         return res
+
+##Time limite exceeded: leetcode
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]*len(temperatures)
+        stack = [] 
+        
+        for curr_day, curr_temp in enumerate(temperatures):
+            #pop until the current day's temp is not warmer than the temp at the top of the stack
+            while stack and curr_temp > temperatures[stack[-1]]:
+                prev_day = stack.pop()
+                res[prev_day] = curr_day - prev_day
+                
+            stack.append(curr_day)
+            
+        return res
