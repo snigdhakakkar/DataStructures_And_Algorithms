be04680810e15e0c6cd39594e346c3532bf43a0b
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-09-18 20:00:00 -0600

    monotonic stack problem

---
+++ Leetcode_Python/Neetcode150/DailyTemperatures.py
@@ -0,0 +1,18 @@
+##Time Limit Exceeded - Neetcode approach
+
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]*len(temperatures)
+        stack = [] 
+        
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+                
+            stack.append([t,i])
+            
+        return res
