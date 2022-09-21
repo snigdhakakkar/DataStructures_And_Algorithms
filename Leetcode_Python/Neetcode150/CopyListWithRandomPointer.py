4c22ed59cb3b9f6aaf1f68edb807b04fdbc474e2
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-09-21 12:42:39 -0400

    use 2 passes of while curr loop. First fill up the oldToCopy hashmap mapping old list to the new list in first pass. Map connections in the second pass. Return oldToCopy[head].

---
+++ Leetcode_Python/Neetcode150/CopyListWithRandomPointer.py
@@ -0,0 +1,27 @@
+
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: Node) -> Node:
+        oldToCopy = {None: None} #hashmap to map the old list to the new one
+        
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            oldToCopy[curr] = copy
+            curr = curr.next
+            
+        curr = head
+        while curr:
+            copy = oldToCopy[curr]
+            copy.next = oldToCopy[curr.next]
+            copy.random = oldToCopy[curr.random]
+            curr = curr.next
+            
+        return oldToCopy[head]
