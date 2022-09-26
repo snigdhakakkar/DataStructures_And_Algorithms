b4a6c5f7d6b4eb5de535772453f635770f1c2d6c
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-09-26 05:54:43 -0400

    iterative DFS

--- Leetcode_Python/Neetcode150/MaximumDepthOfATree.py
+++ Leetcode_Python/Neetcode150/MaximumDepthOfATree.py
@@ -39,3 +39,20 @@ class Solution:
         return level
 
 ##Iterative DFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        stack = [[root, 1]]
+        res = 1
+        
+        while stack:
+            node, depth = stack.pop()
+            
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+                
+        return res
