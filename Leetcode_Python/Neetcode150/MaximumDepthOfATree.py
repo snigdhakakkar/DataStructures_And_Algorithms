76a60fbb89940f06b00de5726fac50f11f81f35f
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-09-26 05:44:33 -0400

    iterative BFS

--- Leetcode_Python/Neetcode150/MaximumDepthOfATree.py
+++ Leetcode_Python/Neetcode150/MaximumDepthOfATree.py
@@ -1,5 +1,8 @@
 ## Recursive DFS
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -12,3 +15,27 @@ class Solution:
         
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
+## Iterative BFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        level = 0
+        
+        q = deque([root])
+        
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                    
+                if node.right:
+                    q.append(node.right)
+                    
+            level += 1
+            
+        return level
+
+##Iterative DFS
