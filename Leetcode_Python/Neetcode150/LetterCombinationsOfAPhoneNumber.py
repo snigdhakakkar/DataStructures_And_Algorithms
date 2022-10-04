7e5f93f0941d130b6e09b094dcaa72c0cf7c36da
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-10-04 14:55:00 -0400

    use backtracking. use a digit to character map to get the characters c mapping to a particular digit. Nested backtrack(i, curStr) fxn. Handle the base case when len(curStr) == len(digits) - the combination has been made. Use a for loop for each character in characters mapped for a digit, call backtrack(i+1, curStr+c). Finally call backtrack(0, ) and return res if digits exist

---
+++ Leetcode_Python/Neetcode150/LetterCombinationsOfAPhoneNumber.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        #time complexity: O(n.4^n) where n is the length of the string
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+                
+        if digits:
+            backtrack(0, "")
+            
+        return res
+            
