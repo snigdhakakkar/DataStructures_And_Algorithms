b557b23ee93e90267e30bae75b6ad5d659fa4776
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-10-09 20:00:00 -0600

    Use BFS for linear time solution. Initialize ROWS, COLS, visit set and deque q. Nested fxn: addRoom(r,c). Outofbound conditions and then add to visit set and append it to q. Nested for loops for row and col to populate all the gates in the queue q and  visit set. Initialize dist. Run while q.For range of len(q): popleft the coordinates. Update rooms[r][c] as dist. Recursively call addRoom for all four directions and update dist outside the for loop

---
+++ Leetcode_Python/Neetcode150/WallsAndGates.py
@@ -0,0 +1,36 @@
+from collections import deque
+from pip import List
+
+
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        ROWS, COLS = len(rooms), len(rooms[0])
+        visit = set()
+        q = deque()
+        
+        def addRoom(r,c):
+            if (r < 0 or r==ROWS or c < 0 or c==COLS or rooms[r][c] == -1 or (r,c) in visit):
+                return
+            visit.add((r,c))
+            q.append([r,c])
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    q.append([r,c])
+                    visit.add((r,c))
+        
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                
+                rooms[r][c] = dist
+                
+                addRoom(r + 1, c)
+                addRoom(r - 1, c)
+                addRoom(r, c + 1)
+                addRoom(r, c - 1)
+                
+                
+            dist += 1
