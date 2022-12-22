58a430dc07d205ed8e9ee33b7196408db02fbb08
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-12-21 20:00:00 -0600

    best time to buy & sell stocks II

--- Leetcode_Python/EasyProblemsLeetcode/Best_Time_To_Buy_And_Sell_Stocks_II.py
+++ Leetcode_Python/EasyProblemsLeetcode/Best_Time_To_Buy_And_Sell_Stocks_II.py
@@ -1,2 +1,15 @@
 ##TIME COMPLEXITY: O(N)
 ##Space complexity: O(1)
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            ##check if next day price is greater than the previous day price
+            if prices[i] >= prices[i - 1]:
+                profit += (prices[i] - prices[i - 1])
+                
+        return profit
