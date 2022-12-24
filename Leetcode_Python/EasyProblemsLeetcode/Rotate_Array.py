a9f0bff57f4d4886d5eca4e57b88c69abf0c8ffc
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-12-23 20:00:00 -0600

    rotate one step at a time approach

--- Leetcode_Python/EasyProblemsLeetcode/Rotate_Array.py
+++ Leetcode_Python/EasyProblemsLeetcode/Rotate_Array.py
@@ -1,3 +1,18 @@
-##Approach 1: Rotate 1 step at a time (k steps):
+##Approach 1: By placing each element at its correct location (moved by 1 step at a time):
     #time complexity: O(nk)
     #space complexity: O(1)
+
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        
+        for i in range(k):
+            previous = nums[-1]
+            for j in range(len(nums)):
+                nums[j], previous = previous, nums[j]
