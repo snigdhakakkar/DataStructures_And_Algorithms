fd806c39b1e1f9452a2dfc5cbc0c75890c844bf0
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-12-24 20:00:00 -0600

    reverse approach

--- Leetcode_Python/EasyProblemsLeetcode/Rotate_Array.py
+++ Leetcode_Python/EasyProblemsLeetcode/Rotate_Array.py
@@ -16,3 +16,29 @@ class Solution:
             previous = nums[-1]
             for j in range(len(nums)):
                 nums[j], previous = previous, nums[j]
+
+##Approach 2: Reverse the entire array first --> then reverse until the k elements and the remaining portion separately
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        l, r = 0, len(nums) - 1
+        
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+            
+        l, r = 0, k - 1
+        
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+            
+        l, r = k, len(nums) - 1
+        
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
