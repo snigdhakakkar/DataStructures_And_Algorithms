29f5d4ebe8fd501a1036385a1b6fbb51a096cd7c
Snigdha Kakkar <snigdhakakkar@gmail.com>
2022-12-30 20:00:00 -0600

    intersection of two arrays

--- Leetcode_Python/EasyProblemsLeetcode/Interesection_Of_Two_ArraysII.py
+++ Leetcode_Python/EasyProblemsLeetcode/Interesection_Of_Two_ArraysII.py
@@ -18,3 +18,7 @@ class Solution:
         return result
 
 ## What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+## If only one of the lists, say nums2 cannot fit in the disk memory, then put all the elements of nums1 in hashMap. Read
+## chunks of data of the array which fits in the memory and record the transactions.
+
+## If it is the case that both the lists do not come in the memory, then sort them externally and compare two elements each at one time.
