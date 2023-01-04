8446cbd3dc590ef5d04898f64c9bbc737421bf98
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-04 14:48:02 -0500

    Use stack and then replace elements in array with popped items

--- Leetcode_Python/EasyProblemsLeetcode/Reverse_String.py
+++ Leetcode_Python/EasyProblemsLeetcode/Reverse_String.py
@@ -13,3 +13,19 @@ class Solution:
         while l < r:
             s[l], s[r] = s[r], s[l]
             l, r = l + 1, r - 1
+
+##Approach 2: Use stack - time complexity: O(N), space complexity: O(N)
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        stack = []
+        
+        for c in s:
+            stack.append(c)
+        
+        i = 0
+        while stack:
+            s[i] = stack.pop()
+            i += 1
