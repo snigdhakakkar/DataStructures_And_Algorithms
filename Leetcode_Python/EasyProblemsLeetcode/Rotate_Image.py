ccc8d853364cdadb732c3ed6a74aa5638096b9a1
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-04 14:34:11 -0500

    second approach: transpose and reflect

--- Leetcode_Python/EasyProblemsLeetcode/Rotate_Image.py
+++ Leetcode_Python/EasyProblemsLeetcode/Rotate_Image.py
@@ -29,3 +29,25 @@ class Solution:
                 
             r -= 1
             l += 1
+
+##Approach 2: Transpose and reflect
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self.transpose(matrix)
+        self.reflect(matrix)
+        
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+                
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
