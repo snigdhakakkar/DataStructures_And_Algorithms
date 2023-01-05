6582d9d0ff7aa5898aa24e7f518085b5f9cb30fe
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-05 15:23:31 -0500

    Check for each string in strs and for each character in the first string. Handle the edge case that the zeroth index string might not be the smallest. Return res if the ith character is different for any string. Update res after each iteration of inner for loop. Return res.

---
+++ Leetcode_Python/EasyProblemsLeetcode/Longest_Common_Prefix.py
@@ -0,0 +1,17 @@
+##time complexity = O(n*m) where n is the average length of the string and m is the no. of strings
+
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+                
+            res = res + strs[0][i]
+            
+        return res
