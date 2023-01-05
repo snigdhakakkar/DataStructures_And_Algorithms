d13f192096e8d1342e214d75a038f0b184c7b1b3
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-05 14:31:45 -0500

    two pointer approach - building alphanum function using ascii code values. and then calling that function to compare the characters at left and right pointers.

--- Leetcode_Python/EasyProblemsLeetcode/Valid_palindrome.py
+++ Leetcode_Python/EasyProblemsLeetcode/Valid_palindrome.py
@@ -7,3 +7,36 @@ class Solution:
             if c.isalnum():
                 newStr = newStr + c.lower()
         return newStr == newStr[::-1]
+
+## Approach 2: Two pointer approach
+# time complexity: O(N)
+# space complexity: O(1)
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ## two pointer approach
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            
+            while r > l and not self.alphanum(s[r]):
+                r -= 1
+                
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+            
+        return True
+        
+        
+        
+    def alphanum(self, c):
+        if (ord('A') <= ord(c) <= ord('Z') or 
+            ord('a') <= ord(c) <= ord('z') or
+            ord('0') <= ord(c) <= ord('9')
+           ):
+            return True
+        return False
