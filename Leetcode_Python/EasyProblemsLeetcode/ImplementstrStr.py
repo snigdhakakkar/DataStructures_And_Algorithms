f3ed1671e4908b24fe23099f92b3518894aa67bc
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-05 15:11:45 -0500

    replacing inner for loop with haystack[i + len(needle)] = = needle

--- Leetcode_Python/EasyProblemsLeetcode/ImplementstrStr.py
+++ Leetcode_Python/EasyProblemsLeetcode/ImplementstrStr.py
@@ -16,3 +16,14 @@ class Solution:
                     return i
                 
         return -1
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle=="":
+            return -1
+        
+        for i in range(len(haystack) + 1 - len(needle)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+                
+        return -1
