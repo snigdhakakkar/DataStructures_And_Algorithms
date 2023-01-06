ad3082d859995a2e6dad1a4404509389dd7406b8
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-06 15:01:18 -0500

    Recursive Traversal with valid range

---
+++ Leetcode_Python/EasyProblemsLeetcode/Validate_Binary_Search_Tree.py
@@ -0,0 +1,27 @@
+## Iterative DFS: O(N^2) time complexity
+
+## Recursive Traversal with Valid Range: O(N) time complexity
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class SolutionI:
+
+    def isValidBST(self, root: TreeNode) -> bool:
+        
+        def valid(node, left, right):
+            if not node:
+                return True
+            
+            if not (node.val < right and node.val > left):
+                return False
+            
+            return (valid(node.left, left, node.val) and
+            valid(node.right, node.val, right))
+            
+        return valid(root, float("-inf"), float("inf"))
+
