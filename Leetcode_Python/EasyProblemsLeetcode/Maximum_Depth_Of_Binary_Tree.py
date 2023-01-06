4a1a640c76d4fd0cfec24951499e7bf1ec4ccf34
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-06 14:46:26 -0500

    Approach 3: Iterative DFS - Use a stack to undertake preorder traversal and compute depth

--- Leetcode_Python/EasyProblemsLeetcode/Maximum_Depth_Of_Binary_Tree.py
+++ Leetcode_Python/EasyProblemsLeetcode/Maximum_Depth_Of_Binary_Tree.py
@@ -10,6 +10,23 @@ class TreeNode:
 
 #Iterative DFS
 
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        stack = [[root, 1]]
+        res = 0
+        
+        while stack:
+            node, depth = stack.pop()
+            
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+                
+        return res
 
 
 #Recursive DFS
