cd5a444972018388d402cee49c6ec34bf1cea5fb
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-06 15:05:34 -0500

    Approach 2: Iterative Traversal with valid range (use a stack)

--- Leetcode_Python/EasyProblemsLeetcode/Validate_Binary_Search_Tree.py
+++ Leetcode_Python/EasyProblemsLeetcode/Validate_Binary_Search_Tree.py
@@ -1,14 +1,34 @@
-## Iterative DFS: O(N^2) time complexity
+## Iterative Traversal with valid range: O(N) time complexity, O(N) space complexity
 
-## Recursive Traversal with Valid Range: O(N) time complexity
-
+import math
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+class SolutionII:
+    def isValidBST(self, root: TreeNode) -> bool:
         
+        if not root:
+            return True
+        
+        stack = [(root, -math.inf, math.inf)]
+        
+        while stack:
+            root, lower, upper = stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            stack.append((root.right, val, upper))
+            stack.append((root.left, lower, val))
+            
+        return True
+
+## Recursive Traversal with Valid Range: O(N) time complexity, O(N) space complexity
 class SolutionI:
 
     def isValidBST(self, root: TreeNode) -> bool:
