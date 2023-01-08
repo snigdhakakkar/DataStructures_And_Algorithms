96cb096195cb4afc59b8516ea6e410705b26c790
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-07 20:00:00 -0600

    bfs implementation

---
+++ Leetcode_Python/EasyProblemsLeetcode/Binary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,33 @@
+## BFS 
+# time complexity: O(N), space complexity: O(N)
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+import collections
+from typing import List
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        q = collections.deque()
+        q.append(root)
+        
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+                
+        return res
