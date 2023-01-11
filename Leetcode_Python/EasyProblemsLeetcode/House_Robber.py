931b7bc6df80619ccce878e609687071779f603a
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-11 14:58:17 -0500

    recursion with memoization

--- Leetcode_Python/EasyProblemsLeetcode/House_Robber.py
+++ Leetcode_Python/EasyProblemsLeetcode/House_Robber.py
@@ -1,3 +1,34 @@
+## Recursion with Memoization: time complexity - O(N), space complexity - O(N)
+
+class Solution:
+    def __init__(self):
+        self.memo = {}
+        
+        
+    def rob(self, nums: List[int]) -> int:
+        
+        self.memo = {}
+        return self.robFrom(0, nums)
+    
+    def robFrom(self, i, nums):
+        
+        ##edge case: no more houses left
+        if i >= len(nums):
+            return 0
+        
+        #return cached value
+        if i in self.memo:
+            return self.memo[i]
+        
+        # Recursive relation evaluation to get the optimal answer.
+        ans = max(self.robFrom(i+1, nums), self.robFrom(i+2, nums) + nums[i])
+        
+        # Cache for future use.
+        self.memo[i] = ans
+        return ans
+
+
+
 ## Optimized DP Approach: time complexity - O(N)
 from typing import List
 
