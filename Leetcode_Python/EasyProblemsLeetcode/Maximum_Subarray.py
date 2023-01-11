0c321510ac60197fa5c1e1e8c2455c2de0463ab6
Snigdha Kakkar <snigdhakakkar@gmail.com>
2023-01-11 14:37:33 -0500

    bruteforce approach

--- Leetcode_Python/EasyProblemsLeetcode/Maximum_Subarray.py
+++ Leetcode_Python/EasyProblemsLeetcode/Maximum_Subarray.py
@@ -2,6 +2,20 @@
 
 ## Approach 2: Saving the curSum and using it to compute another sum wherein it is a subpart - time complexity: O(N^2)
 
+import math
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_subarray = -math.inf
+        for i in range(len(nums)):
+            current_subarray = 0
+            for j in range(i, len(nums)):
+                current_subarray += nums[j]
+                max_subarray = max(max_subarray, current_subarray)
+        
+        return max_subarray
+
 ## Approach 3: Ignore negative prefixes. Keep positive prefixes. 
 
 from typing import List
